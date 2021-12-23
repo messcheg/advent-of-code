dfc6ed66688221c1916b8cb7448a011d3931ef2e
Gert-Jan Messchendorp <messcheg@gmail.com>
2021-12-23 08:38:18 +0100

    Other Marvelous but way too slow solution.

--- AdventOfCode2021/Day22/Program.cs
+++ AdventOfCode2021/Day22/Program.cs
@@ -5,7 +5,7 @@ void Run()
     string inputfile = @"..\..\..\example_input.txt";
     //string inputfile = @"..\..\..\real_input.txt";
     long supposedanswer1 = 590784;
-    long supposedanswer2 = 0000;
+    long supposedanswer2 = 2758514936282235;
 
     var S = File.ReadAllLines(inputfile).ToList();
     long answer1 = 0;
@@ -13,7 +13,7 @@ void Run()
 
     var instructions = new (bool turnOn, (int from, int to) x, (int from, int to) y, (int from, int to) z)[S.Count];
     int i = 0;
-    foreach(string s in S)
+    foreach (string s in S)
     {
         var s1 = s.Split(' ');
         instructions[i].turnOn = s1[0] == "on";
@@ -23,54 +23,56 @@ void Run()
         instructions[i].x.to = int.Parse(s2[1]);
         s2 = s1[1].Split('=')[1].Split("..");
         instructions[i].y.from = int.Parse(s2[0]);
-        instructions[i].y.to = int.Parse(s2[1]); 
+        instructions[i].y.to = int.Parse(s2[1]);
         s2 = s1[2].Split('=')[1].Split("..");
         instructions[i].z.from = int.Parse(s2[0]);
         instructions[i].z.to = int.Parse(s2[1]);
         i++;
     }
 
-    var minicubes = new List<Cube>();
-    for(int j=0; j<instructions.Length; j++)
+    i = 0;
+    CubeArea cube = new CubeArea() { size = 1 , restriction = (true, -50,50)};
+    foreach (var instr in instructions)
     {
-        var c = instructions[j];
-        if (c.turnOn)
+        if (instr.turnOn)
         {
-            var cubestoadd = new Queue<Cube>();
-            cubestoadd.Enqueue(new Cube() { from = (c.x.from, c.y.from, c.z.from), to = (c.x.to, c.y.to, c.z.to)});
-            while (cubestoadd.Count > 0)
-            {
-                var currentcube = cubestoadd.Dequeue();
-                bool no_overlaps = true;
-                foreach (var mini in minicubes)
-                {
-                    var res = currentcube.CutResult(mini);
-                    if (res.Length > 1)
-                    {
-                        no_overlaps = false;
-                        foreach (var cube in res) cubestoadd.Enqueue(cube);
-                    }
-                }
-                if (no_overlaps) minicubes.Add(currentcube);
-            }
+            cube.Fit(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
+            cube.Add(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
         }
         else
         {
-            var newMinicubes = new List<Cube>();
-            var clearArea = new Cube() { from = (c.x.from, c.y.from, c.z.from), to = (c.x.to, c.y.to, c.z.to) };
-            foreach(var currentcube in minicubes)
-            {
-               var res = currentcube.CutResult(clearArea);
-               foreach( var c1 in res)
-                {
-                    newMinicubes.Add(c1);
-                }
-            }
-            minicubes = newMinicubes;
-        }
+            cube.Delete(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
         }
 
-    answer1 = minicubes.Sum(c => c.Count());
+        Console.WriteLine(S[i]);
+        Console.WriteLine("Counter: " + cube.Count());
+
+        i++;
+    }
+
+    answer1 = cube.Count();
+
+    i = 0;
+    cube = new CubeArea() { size = 1, restriction = (false, 0, 0) };
+    foreach (var instr in instructions)
+    {
+        if (instr.turnOn)
+        {
+            cube.Fit(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
+            cube.Add(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
+        }
+        else
+        {
+            cube.Delete(instr.x.from, instr.y.from, instr.z.from, instr.x.to, instr.y.to, instr.z.to);
+        }
+
+        Console.WriteLine(S[i]);
+        Console.WriteLine("Counter: " + cube.Count());
+
+        i++;
+    }
+
+    answer2 = cube.Count();
 
     w(1, answer1, supposedanswer1);
     w(2, answer2, supposedanswer2);
@@ -91,160 +93,208 @@ static void w<T>(int number, T val, T supposedval)
     Console.WriteLine(sv);
     Console.ForegroundColor = previouscolour;
 }
-
-class Cube
-{
-    public (int x, int y, int z) from;
-    public (int x, int y, int z) to;
-    public bool Overlaps(Cube other)
-    {
-        return Between(this.from, other.from, other.to) || Between(this.to, other.from, other.to);
-    }
-    public bool EnclosedBy(Cube other)
-    {
-        return Between(this.from, other.from, other.to) && Between(this.to, other.from, other.to);
-    }
-    public bool Encloses(Cube other)
-    {
-        return Between(other.from, this.from, this.to) && Between(other.to, this.from, this.to);
-    }
-
-    public long Count()
-    {
-        return (to.x - from.x + 1) * (to.y - from.y + 1) * (to.z - from.z + 1);
-    }
-    public Cube[] CutResult (Cube other)
-    {
-        if (EnclosedBy(other)) return new Cube[0];
-        
-        List<Cube> result = new List<Cube>();
-        result.Add(this);
-        result = CutX(result, other);
-        result = CutY(result, other);
-        result = CutZ(result, other);
-        return result.Where(c => !c.EnclosedBy(other)).ToArray();
-    }
-    private List<Cube> CutX(List<Cube> input, Cube other)
-    {
-        List<Cube> output = new List<Cube>();
-        foreach (var c in input)
-        {
-            if (Between(other.from.x, c.from.x, c.to.x))
-            {
-                if (Between(other.to.x, c.from.x, c.to.x))
-                {
-                    if (c.from.x != other.from.x) output.Add(new Cube() { from = c.from, to = (other.from.x -1, c.to.y, c.to.z) });
-                    output.Add(new Cube() { from = (other.from.x, c.from.y, c.from.z) , to = (other.to.x, c.to.y, c.to.z) });
-                    if (c.to.x != other.to.x) output.Add(new Cube() { from = (other.to.x + 1, c.from.y, c.from.z), to = c.to});
-                }
-                else
-                {
-                    if (c.from.x != other.from.x)  output.Add(new Cube() { from = c.from, to = (other.from.x - 1, c.to.y, c.to.z) });
-                    output.Add(new Cube() { from = (other.from.x, c.from.y, c.from.z), to = c.to});
-                }
-            }
-            else if (Between(other.to.x, c.from.x, c.to.x))
-            {
-                output.Add(new Cube() { from = c.from, to = (other.to.x, c.to.y, c.to.z) });
-                if (c.to.x != other.to.x) output.Add(new Cube() { from = (other.to.x + 1, c.from.y, c.from.z), to = c.to });
-            }
-            else output.Add(c);
-        }
-        return output;
-    }
-    private List<Cube> CutY(List<Cube> input, Cube other)
-    {
-        List<Cube> output = new List<Cube>();
-        foreach (var c in input)
-        {
-            if (Between(other.from.y, c.from.y, c.to.y))
-            {
-                if (Between(other.to.y, c.from.y, c.to.y))
-                {
-                    if (c.from.y != other.from.y) output.Add(new Cube() { from = c.from, to = (c.to.x, other.from.y - 1, c.to.z) });
-                    output.Add(new Cube() { from = (c.from.x, other.from.y, c.from.z), to = (c.to.x, other.to.y, c.to.z) });
-                    if (c.to.y != other.to.y) output.Add(new Cube() { from = (c.from.x, other.to.y + 1, c.from.z), to = c.to });
-                }
-                else
-                {
-                    if (c.from.y != other.from.y) output.Add(new Cube() { from = c.from, to = (c.to.x, other.from.y - 1, c.to.z) });
-                    output.Add(new Cube() { from = (c.from.x, other.from.y, c.from.z), to = c.to });
-                }
-            }
-            else if (Between(other.to.y, c.from.y, c.to.y))
-            {
-                output.Add(new Cube() { from = c.from, to = (c.to.x, other.to.y, c.to.z) });
-                if (c.to.y != other.to.y) output.Add(new Cube() { from = (c.from.x, other.to.y + 1, c.from.z), to = c.to });
-            }
-            else output.Add(c);
-        }
-        return output;
-    }
-
-    private List<Cube> CutZ(List<Cube> input, Cube other)
-    {
-        List<Cube> output = new List<Cube>();
-        foreach (var c in input)
-        {
-            if (Between(other.from.z, c.from.z, c.to.z))
-            {
-                if (Between(other.to.z, c.from.z, c.to.z))
-                {
-                    if (c.from.z != other.from.z) output.Add(new Cube() { from = c.from, to = (c.to.x, c.to.y, other.from.z - 1) });
-                    output.Add(new Cube() { from = (c.from.x, c.from.y, other.from.z), to = (c.to.x, c.to.y, other.to.z) });
-                    if (c.to.z != other.to.z) output.Add(new Cube() { from = (c.from.x, c.from.y, other.to.z + 1), to = c.to });
-                }
-                else
-                {
-                    if (c.from.z != other.from.z) output.Add(new Cube() { from = c.from, to = (c.to.x, c.to.y, other.from.z - 1) });
-                    output.Add(new Cube() { from = (c.from.x, c.from.y, other.from.z), to = c.to });
-                }
-            }
-            else if (Between(other.to.z, c.from.z, c.to.z))
-            {
-                output.Add(new Cube() { from = c.from, to = (c.to.x, c.to.y, other.to.z) });
-                if (c.to.z != other.to.z) output.Add(new Cube() { from = (c.from.x, c.from.y, other.to.z + 1), to = c.to });
-            }
-            else output.Add(c);
-        }
-        return output;
-    }
-    
-
-    public bool Between((int x, int y, int z) a, (int x, int y, int z) from, (int x, int y, int z) to)
-    {
-        return Between(a.x, from.x, to.x) &&
-           Between(a.y, from.y, to.y) &&
-           Between(a.z, from.z, to.z);
-    }
-    public bool Between(int a, int from, int to)
-    {
-        return from <= a && to >= a;
-    }
-}
-
 class CubeArea
 {
-    public int level;
-    public int size;
-    public int x;
-    public int y;
-    public int z;
+    public int size = 1;
+    public (int x, int y, int z) offset = (0,0,0);  
+    public bool isOn;
+    public CubeArea[,,] subareas;
 
-    public bool isOneColor;
-    public CubeArea[2,2,2] subareas;
-    public int color;
-
-    public int Add(int xfrom, int yfrom, int zfrom, int xto, int yto, int zto)
+    public (bool doit, int min, int max) restriction;
+   
+    public int Restrict(int k)
     {
-        while (! (x <= xfrom && x + size > xto && 
-            y <= yfrom && y + size > yto &&
-            z <= yfrom && z + size > zto))
+        if (!restriction.doit) return k;
+        if (k < restriction.min) return restriction.min;
+        if (k > restriction.max) return restriction.max;
+        return k;
+    }
+    public void Fit(int xfrom, int yfrom, int zfrom, int xto, int yto, int zto)
+    {
+        while (Restrict(xfrom) < offset.x || Restrict(xto) >= offset.x + size ||
+               Restrict(yfrom) < offset.y || Restrict(yto) >= offset.y + size ||
+               Restrict(zfrom) < offset.z || Restrict(zto) >= offset.z + size)
         {
-            var newsubcubes = new CubeArea[2,2,2];
-            for (i)
+            CubeArea[,,] newsubcubes;
+            if (isOn || subareas != null)
+            {
+                newsubcubes = new CubeArea[3, 3, 3];
+                newsubcubes[1, 1, 1] = new CubeArea() { size = size, isOn = isOn, subareas = subareas, offset = offset, restriction = restriction };
+                this.subareas = newsubcubes;
+            }
+            offset = (offset.x - size, offset.y - size, offset.z - size);
+            size = size * 3;
+        }
 
+    }
+    public void Add(int xfrom, int yfrom, int zfrom, int xto, int yto, int zto)
+    {
+        if (NotInRestrictionZone(xfrom, yfrom, zfrom, xto, yto, zto)) return;
+        xfrom = Restrict(xfrom); 
+        yfrom = Restrict(yfrom); 
+        zfrom = Restrict(zfrom); 
+        xto   = Restrict(xto  ); 
+        yto   = Restrict(yto  );
+        zto   = Restrict(zto  );
+
+        if (isOn) return;
+        var parameters = new int[][] { new int[] {offset.x, xfrom , xto }, 
+                     new int [] {offset.y, yfrom, yto }, 
+                     new int [] {offset.z, zfrom, zto } }; 
+
+        var skipareas = new bool[3, 3]; 
+        bool on = true;
+        for (int i = 0; i < 3; i++)
+        {
+            int a = parameters[i][0];
+            int from = parameters[i][1];
+            int to = parameters[i][2];
+            if (a < from || to < a + size - 1) on = false;
+            int start = (from - a) * 3 / size;
+            int end = (to - a) * 3 / size;
+            skipareas[i, 0] = start > 0;
+            skipareas[i, 1] = start > 1 || end < 1;
+            skipareas[i, 2] = end < 2;
+        }
+
+        if (on)
+        {
+            isOn = true;
+            this.subareas = null;
+        }
+        else
+        {
+            for (int x = 0; x<3;x++)
+                for (int y = 0; y < 3; y++)
+                    for (int z = 0; z < 3; z++)
+                    {
+                        if (!skipareas[0,x] && !skipareas[1,y] && !skipareas[2,z])
+                        {
+                            if (subareas == null) subareas = new CubeArea[3, 3, 3];
+                            CubeArea a = subareas[x, y, z];
+                            int sd3 = size / 3;
+                            if (a == null) 
+                                a = subareas[x, y, z] = new CubeArea()
+                                {
+                                    size = sd3,
+                                    offset = (offset.x + x * sd3, offset.y + y * sd3, offset.z + z * sd3),
+                                    isOn = false || sd3==1,
+                                    subareas = null
+                                };
+                            if (sd3 > 1 && !a.isOn) a.Add(xfrom, yfrom, zfrom, xto, yto, zto);
+                        }
+                    }
+            CheckAllOn();
         }
     }
 
+    private bool NotInRestrictionZone(int xfrom, int yfrom, int zfrom, int xto, int yto, int zto)
+    {   if (restriction.doit) return 
+            (
+               xfrom < restriction.min && xto < restriction.min
+            || yfrom < restriction.min && yto < restriction.min
+            || zfrom < restriction.min && zto < restriction.min
+            || xfrom > restriction.max && xto > restriction.max
+            || yfrom > restriction.max && yto > restriction.max
+            || zfrom > restriction.max && zto > restriction.max
+            );
+        return false;
+    }
+
+    public void CheckAllOn()
+    {
+        if (subareas == null) return;
+        for (int x = 0; x < 3; x++)
+            for (int y = 0; y < 3; y++)
+                for (int z = 0; z < 3; z++)
+                {
+                    if (subareas[x, y, z] == null || !subareas[x, y, z].isOn) return;
+                }
+        isOn = true;
+        subareas = null;
+    }
+
+    public void Delete(int xfrom, int yfrom, int zfrom, int xto, int yto, int zto)
+    {
+        if (subareas == null && !isOn) return;
+        var parameters = new int[][] { new int[] {offset.x, xfrom , xto },
+                     new int [] {offset.y, yfrom, yto },
+                     new int [] {offset.z, zfrom, zto } };
+
+        var skipareas = new bool[3, 3];
+        bool off = true;
+        for (int i = 0; i < 3; i++)
+        {
+            int a = parameters[i][0];
+            int from = parameters[i][1];
+            int to = parameters[i][2];
+            if (a < from || to < a + size - 1) off = false;
+            int start = (from - a) * 3 / size;
+            int end = (to - a) * 3 / size;
+            skipareas[i, 0] = start > 0;
+            skipareas[i, 1] = start > 1 || end < 1;
+            skipareas[i, 2] = end < 2;
+        }
+
+        if (off)
+        {
+            isOn = false;
+            this.subareas = null;
+        }
+        else
+        {
+            if (subareas == null && isOn && size > 1)
+            {
+                subareas = new CubeArea[3, 3, 3];
+                int sd3 = size / 3;
+                for (int x = 0; x < 3; x++)
+                    for (int y = 0; y < 3; y++)
+                        for (int z = 0; z < 3; z++)
+                        {
+                            subareas[x, y, z] = new CubeArea()
+                            {
+                                size = sd3,
+                                offset = (offset.x + x * sd3, offset.y + y * sd3, offset.z + z * sd3),
+                                isOn = true,
+                                subareas = null
+                            };
+                        }
+                isOn = false;
+            }
+        
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++)
+                    for (int z = 0; z < 3; z++)
+                    {
+                        if (!skipareas[0, x] && !skipareas[1, y] && !skipareas[2, z])
+                        {
+                            if (subareas != null)
+                            {
+                                CubeArea a = subareas[x, y, z];
+                                if (a != null) a.Delete(xfrom, yfrom, zfrom, xto, yto, zto);
+                            }
+                        }
+                    }
+            CheckAllOff();
+        }
+    }
+    public void CheckAllOff()
+    {
+        if (subareas == null) return;
+        for (int x = 0; x < 3; x++)
+            for (int y = 0; y < 3; y++)
+                for (int z = 0; z < 3; z++)
+                {
+                    if (subareas[x, y, z] != null) return;
+                }
+        isOn = false;
+        subareas = null;
+    }
+    public long Count()
+    {
+        if (this.subareas == null) return isOn ? (long)size * size * size : 0;
+        long cnt = 0;
+        foreach (var sub in subareas) if (sub != null) cnt += sub.Count();
+        return cnt;
+    }
 }
--- AdventOfCode2021/Day23/Program.cs
+++ AdventOfCode2021/Day23/Program.cs
@@ -4,7 +4,7 @@ void Run()
 {
     string inputfile = @"..\..\..\example_input.txt";
     //string inputfile = @"..\..\..\real_input.txt";
-    long supposedanswer1 = 0000;
+    long supposedanswer1 = 12521;
     long supposedanswer2 = 0000;
 
     var S = File.ReadAllLines(inputfile).ToList();
@@ -17,11 +17,18 @@ void Run()
 
         i++;
     }
+    
+    var room = new int[][] { new int[] {1,0 }, new int[]{2,3 }, new int[]{1,2 }, new int[]{3,1 } };
+    //var room = new int[][] { new int[] {3,3 }, new int[]{2,2 }, new int[]{0,1 }, new int[]{1,0 } };
+    int[] energy_usage = new int[] { 1, 10, 100, 1000 };
+    int[] hallway = new int[11]; 
+
+
 
     w(1, answer1, supposedanswer1);
     w(2, answer2, supposedanswer2);
 }
-
+enum pod { a = 0, b=1, c = 2, d = 3}
 static void w<T>(int number, T val, T supposedval)
 {
     string? v = (val == null) ? "(null)" : val.ToString();
