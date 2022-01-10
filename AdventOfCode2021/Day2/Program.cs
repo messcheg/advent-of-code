877aa1b7ef2dce40cbc795e2bb29bacdf7f1c88f
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-01-10 21:16:47 +0100

    optimized

--- AdventOfCode2021/Day2/Program.cs
+++ AdventOfCode2021/Day2/Program.cs
@@ -1,29 +1,29 @@
-﻿//string inputfile = @"..\..\..\example_input.txt";
+﻿string inputfile = @"..\..\..\example_input.txt";
 //string inputfile = @"..\..\..\real_input.txt";
-string inputfile = @"..\..\..\niels_input.txt";
+//string inputfile = @"..\..\..\niels_input.txt";
 
 var instructions = File.ReadAllLines(inputfile).ToList();
 int horizontal = 0;
 int vertical_two = 0;
 int aim = 0;
 
-foreach(var  instruction in instructions)
+foreach(var instruction in instructions)
 {
     var split = instruction.Split(' ');
-    string command = split[0];
+    char command = split[0][0];
     var amount = int.Parse(split[1]);
-    if (command == "forward")
+    switch (command)
     {
-        horizontal += amount;
-        vertical_two += aim * amount;
-    }
-    else if (command == "down")
-    {
-        aim += amount;
-    }
-    else if (command == "up")
-    {
-        aim -= amount;
+        case 'f':
+            horizontal += amount;
+            vertical_two += aim * amount;
+            break;
+        case 'd':
+            aim += amount;
+            break;
+        default:
+            aim -= amount;
+            break;
     }    
 }
 
