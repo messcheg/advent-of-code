c276f46203aff6e61a0fde0a31cc6fc072f940b8
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-01-22 20:46:25 +0100

    Added Day02 and improved Day01

--- AdventOfCode2019/Day01/Program.fs
+++ AdventOfCode2019/Day01/Program.fs
@@ -4,21 +4,18 @@ open System
 // let filename = "..\\..\\..\\example_input.txt"
 let filename = "..\\..\\..\\real_input.txt"
 
-let fuel mass = (mass / 3) - 2
+let fuel mass : int = (mass / 3) - 2 : int
 
-let sum = File.ReadLines(filename) |>  Seq.map System.Int32.Parse |>
-     Seq.map fuel |>
-     Seq.sum 
+let sum (ff : int -> int ) = File.ReadLines(filename) |>  
+            Seq.map System.Int32.Parse |>
+            Seq.map ff |>
+            Seq.sum 
 
-printfn "Answer1: %d" sum ;;
+printfn "Answer1: %d" (sum fuel)
 
 let rec fuel2 mass = 
     if mass < 8 then 0
     else (fuel mass) + fuel2 (fuel mass)
     
-let sum2 = File.ReadLines(filename) |>  Seq.map System.Int32.Parse |>
-    Seq.map fuel2 |>
-    Seq.sum 
-
-printfn "Answer2: %d" sum2 ;;
+printfn "Answer2: %d" (sum fuel2)
 
---
+++ AdventOfCode2019/Day02/Program.fs
@@ -0,0 +1,50 @@
+﻿
+open System.IO
+open System
+
+// let filename = "..\\..\\..\\example_input.txt"
+let filename = "..\\..\\..\\real_input.txt"
+
+let inp = File.ReadLines(filename) |> 
+            Seq.head |> 
+            fun s -> s.Split(',') |>
+            Seq.map Int32.Parse 
+
+
+let inparr = Seq.toArray inp
+
+let get4 arrInp i =
+    ( Array.get arrInp i,
+    Array.get arrInp (Array.get arrInp (i+1)),
+    Array.get arrInp (Array.get arrInp (i+2)),
+    Array.get arrInp (i+3))
+
+let part2init arrInp a b = 
+    Array.set arrInp 1 a
+    Array.set arrInp 2 b
+    arrInp
+
+let dorun arrInp =
+    let mutable i : int = 0
+    while (Array.get arrInp i) <> 99 do
+        match (get4 arrInp i) with
+            | ( a , b , c , d ) when a = 1 -> Array.set arrInp d (b+c)
+            | ( a , b , c , d ) when a = 2 -> Array.set arrInp d (b*c)
+        i <- i + 4
+    Array.get arrInp 0
+
+printfn "Answer1: %d" (dorun (part2init (Array.copy inparr) 12 2))
+
+let determine arrInp =
+    let mutable p = 0
+    let mutable q = 0
+    while (dorun (part2init (Array.copy inparr) p q)) <> 19690720 do
+        p <- p + 1
+        if p > 99 then
+            q <- q + 1
+            p <- 0
+    p * 100 + q
+
+printfn "Answer2: %d" (determine inparr)
+
+    
