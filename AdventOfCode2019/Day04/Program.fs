0b2069a70a081b804e7d6cfc3ec4b7288ddaf907
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-01-24 12:12:45 +0100

    Added Day04 part 2

--- AdventOfCode2019/Day04/Program.fs
+++ AdventOfCode2019/Day04/Program.fs
@@ -1,14 +1,16 @@
 ﻿let inputfrom = 206938 
 let inputuntil = 679128
 
-let rec checkeqpair a (b : string) =
+let rec checkeqpair a (b : string) a2 ign =
     let l = String.length b
-    if a = b[0] then true
-    elif l > 1 then checkeqpair b[0] b[1.. l - 1]
+    if a = b[0] then
+        if not a2 then true
+        else (a <> ign && (l < 2 || a <> b[1])) || (l > 2 && checkeqpair b[0] b[1.. l - 1] a2 a)
+    elif l > 1 then checkeqpair b[0] b[1.. l - 1] a2 ign
     else false
 
-let containseqpair (b:string) =
-    checkeqpair b[0] b[1.. String.length b - 1]
+let containseqpair (b:string) a2 =
+    checkeqpair b[0] b[1.. String.length b - 1] a2
 
 let rec checkincreasing a (b : string) =
    let l = String.length b
@@ -20,16 +22,20 @@ let rec checkincreasing a (b : string) =
 let allincreasing (b:string) = 
     checkincreasing b[0] b[1.. String.length b - 1]
 
-let meetscriteria num =
+let meetscriteria num a2 =
     let strnum = num.ToString()
-    containseqpair strnum && allincreasing strnum
+    containseqpair strnum a2 ' '
+    && allincreasing strnum 
+  
     
-let numberofvalidkeys =
+let numberofvalidkeys a2 =
     let mutable cnt = 0
     for i = inputfrom to inputuntil do
-        if meetscriteria i then cnt <- cnt + 1
+        if meetscriteria i a2 then cnt <- cnt + 1
     cnt
 
-let answer1 = numberofvalidkeys  
+let answer1 = numberofvalidkeys false
+let answer2 = numberofvalidkeys true
 
 printfn "Answer1: %d" answer1
+printfn "Answer2: %d" answer2
