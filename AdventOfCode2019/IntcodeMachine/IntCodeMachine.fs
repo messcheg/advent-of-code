d1598528ade40e0b4fb7ca9068a499be84594535
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-01-31 23:12:47 +0100

    Added Day07 part2

--- AdventOfCode2019/IntcodeMachine/IntCodeMachine.fs
+++ AdventOfCode2019/IntcodeMachine/IntCodeMachine.fs
@@ -9,29 +9,29 @@ module Intcode =
         Seq.head |> 
         (fun s -> 
             s.Split(',') |>
-            Seq.map Int32.Parse) |> 
+            Seq.map Int64.Parse) |> 
         Seq.toArray
     
     let indirect arrInp i = 
         Array.get arrInp (Array.get arrInp i)   
     
-    let getOpcode (arrInp : int[]) i =
+    let getOpcode (arrInp : int64[]) i =
         let num = Array.get arrInp i
-        ( num / 10000,
-         (num % 10000) / 1000,
-         (num % 1000) / 100,
-          num % 100)
+        ( num / 10000L,
+         (num % 10000L) / 1000L,
+         (num % 1000L) / 100L,
+          num % 100L)
     
-    let getValue arrInp mode i =
+    let getValue (arrInp : int64[]) mode i =
         let p = Array.get arrInp i 
-        if mode = 0 then Array.get arrInp p
+        if mode = 0 then Array.get arrInp (int p)
         else p
     
-    let setValue arrInp mode i v =
+    let setValue (arrInp : int64[]) mode i v =
         let p = 
             if mode = 0 then Array.get arrInp i
             else  Array.get arrInp i // set is always indirect
-        Array.set arrInp p v 
+        Array.set arrInp (int p) v 
     
     let opr opc a b =
         if opc = 1 then a + b
@@ -40,24 +40,53 @@ module Intcode =
         elif opc = 8 then  if a = b then 1 else 0
         else 0
     
-    let dorun arrInp getInput =
+    let doruntilout (arrInp : int64[] ) (getInput : int64[]) pc  =
+        let mutable i : int = pc
+        let mutable out = (0L,0)
+        let mutable inpcnt = 0
+        while (Array.get arrInp i) <> 99 && out = (0L,0) do
+            match (getOpcode arrInp i) with
+                | ( a , b , c , de ) when de = 1 || de = 2 || de = 7 || de = 8 -> 
+                    setValue arrInp (int a) (i+3) (opr (int de) (int (getValue arrInp (int c) (i+1))) (int (getValue arrInp (int b) (i+2))))
+                    i <- i + 4
+                | ( a , b , c , de ) when de = 3 ->
+                    setValue arrInp (int c) (i+1) getInput[inpcnt]
+                    inpcnt <- inpcnt + 1
+                    i <- i + 2
+                | ( a , b , c , de ) when de = 4 ->
+                    out <- (getValue arrInp (int c) (i+1), i + 2)
+                    i <- i + 2
+                | ( a , b , c , de ) when de = 5 || de = 6 ->
+                    let cond = getValue arrInp (int c) (i+1)
+                    if de = 6 && cond = 0 || de = 5 && cond > 0 then i <- int (getValue arrInp (int b) (i+2))
+                    else i <- i + 3
+                | _ -> i <- i + 1
+        out
+
+    let dorun1 (arrInp : int64[] ) (getInput : int64[]) =
         let mutable i : int = 0
-        let mutable outp = 0
+        let mutable inpcnt = 0
+        let mutable outp : int64[] = [||]
         while (Array.get arrInp i) <> 99 do
             match (getOpcode arrInp i) with
                 | ( a , b , c , de ) when de = 1 || de = 2 || de = 7 || de = 8 -> 
-                    setValue arrInp a (i+3) (opr de (getValue arrInp c (i+1)) (getValue arrInp b (i+2)))
+                    setValue arrInp (int a) (i+3) (opr (int de) (int (getValue arrInp (int c) (i+1))) (int (getValue arrInp (int b) (i+2))))
                     i <- i + 4
                 | ( a , b , c , de ) when de = 3 ->
-                    setValue arrInp c (i+1) getInput
+                    setValue arrInp (int c) (i+1) getInput[inpcnt]
+                    inpcnt <- inpcnt + 1
                     i <- i + 2
                 | ( a , b , c , de ) when de = 4 ->
-                    outp <- getValue arrInp c (i+1)
+                    outp <- Array.append outp [|getValue arrInp (int c) (i+1)|]
                     i <- i + 2
                 | ( a , b , c , de ) when de = 5 || de = 6 ->
-                    let cond = getValue arrInp c (i+1)
-                    if de = 6 && cond = 0 || de = 5 && cond > 0 then i <- getValue arrInp b (i+2)
+                    let cond = getValue arrInp (int c) (i+1)
+                    if de = 6 && cond = 0 || de = 5 && cond > 0 then i <- int (getValue arrInp (int b) (i+2))
                     else i <- i + 3
                 | _ -> i <- i + 1
         outp
     
+    let dorun arrInp (getInput : int) =
+        let res = dorun1 arrInp [| getInput |]
+        if res = [||] then 0L
+        else res[0]
