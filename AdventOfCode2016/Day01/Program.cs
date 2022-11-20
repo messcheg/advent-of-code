b09e3efc9d310d998c11272a26f5c9309f584abf
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-11-20 12:30:06 +0100

    Added answer 2

--- AdventOfCode2016/Day01/Program.cs
+++ AdventOfCode2016/Day01/Program.cs
@@ -9,34 +9,51 @@ void Run()
 
     var S = File.ReadAllLines(inputfile).ToList();
     long answer1 = 0;
-    long answer2 = 0;
+    long answer2 = -1;
     var S1 = S[0].Split(", ");
 
     int direction = 0;
     int blockN = 0;
     int blockE = 0;
+    HashSet<(int N, int E)> visited = new HashSet<(int N, int E)> ();
+   
+
     for (int i = 0; i < S1.Length; i++)
     {
+        var prevN = blockN;
+        var prevE = blockE;
+
         char turn = S1[i][0];
-        int steps = S1[i][1] - '0';
-        if (turn == 'L')
-        {
-            if (direction == 0) blockE -= steps;
-            else if (direction == 1) blockN += steps;
-            else if (direction == 2) blockE += steps;
-            else if (direction == 3) blockN -= steps;
-            direction--;
-            if (direction < 0) direction = 3;
-        }
-        else
-        {
-            if (direction == 0) blockE += steps;
-            else if (direction == 1) blockN -= steps;
-            else if (direction == 2) blockE -= steps;
-            else if (direction == 3) blockN += steps;
-            direction++;
-            if (direction > 3) direction = 0;
-        }
+        int steps = int.Parse(S1[i][1..]);
+        int turneffect = (turn == 'L') ? -1 : 1;
+        steps = turneffect * steps;
+        
+        if (direction == 0) blockE += steps;
+        else if (direction == 1) blockN -= steps;
+        else if (direction == 2) blockE -= steps;
+        else if (direction == 3) blockN += steps;
+        direction += turneffect;
+        if (direction > 3) direction = 0;
+        else if (direction < 0) direction = 3;
+         
+
+        var ndir = prevN <= blockN ? 1 : -1;
+        var edir = prevE <= blockE ? 1 : -1;
+
+        bool first = true;
+        for (int n = prevN; n != blockN + ndir; n += ndir)
+            for (int e = prevE; e != blockE + edir; e += edir)
+            {
+                if (!first)
+                {
+                    if (answer2 == -1 && visited.Contains((n, e)))
+                    {
+                        answer2 = Math.Abs(n) + Math.Abs(e);
+                    }
+                    visited.Add((n, e));
+                }
+                first = false;
+            }
     }
 
     answer1 = Math.Abs(blockN) + Math.Abs(blockE);
