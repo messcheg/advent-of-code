3c7454008f1ccd9189cd84ae66394fc8baedb22a
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-02 06:38:52 +0100

    Added 2022 Day02

--- AdventOfCode2022/Day02/Program.cs
+++ AdventOfCode2022/Day02/Program.cs
@@ -2,9 +2,9 @@
 
 void Run()
 {
-    string inputfile = @"..\..\..\example_input.txt";
-    //string inputfile = @"..\..\..\real_input.txt";
-    long supposedanswer1 = 0000;
+    //string inputfile = @"..\..\..\example_input.txt";
+    string inputfile = @"..\..\..\real_input.txt";
+    long supposedanswer1 = 15;
     long supposedanswer2 = 0000;
 
     var S = File.ReadAllLines(inputfile).ToList();
@@ -14,7 +14,84 @@ void Run()
 
     for (int i = 0; i < S.Count; i++)
     {
+        char a = S[i][0];
+        char b = S[i][2];
+        int point1 = (int)(b- 'W');
+        int point2 = 0;
+        int point1a = 0;
+        int point2a = 0;
+        if (a=='A' )
+        {
+            if (b == 'X')
+            {
+                point2 = 3;
+                point2a = 0;
+                point1a = 3;
+            }
+            else if (b == 'Y')
+            {
+                point2 = 6;
+                point2a = 3;
+                point1a = 1;
+            }
+            else
+            {
+                point2 = 0;
+                point2a = 6;
+                point1a = 2;
+            }
+        }
+        else if (a=='B')
+        {
 
+            if (b == 'X')
+            {
+                point2 = 0;
+                point2a = 0;
+                point1a = 1;
+            }
+            else if (b == 'Y')
+            {
+                point2 = 3;
+                point2a = 3;
+                point1a = 2;
+            }
+            else
+            {
+                point2 = 6;
+                point2a = 6;
+                point1a = 3;
+            }
+
+        }
+        else
+        {
+            if (b == 'X')
+            {
+                point2 = 6;
+                point2a = 0;
+                point1a = 2;
+            }
+            else if (b == 'Y')
+            {
+                point2 = 0;
+                point2a = 3;
+                point1a = 3;
+            }
+            else
+            {
+                point2 = 3;
+                point2a = 6;
+                point1a = 1;
+            }
+
+            if (b == 'X') point2 = 6;
+            else if (b == 'Y') point2 = 0;
+            else point2 = 3;
+
+        }
+        answer1 += point1 + point2;
+        answer2 += point1a + point2a;
     }
 
 
