3f3a4ae7726d85b76baf5561ac514561a0739a55
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-04 06:16:08 +0100

    Added 2022 Day03

--- AdventOfCode2022/Day03/Program.cs
+++ AdventOfCode2022/Day03/Program.cs
@@ -1,4 +1,6 @@
-﻿Run(@"..\..\..\example_input.txt");
+﻿using System.Runtime.Serialization;
+
+Run(@"..\..\..\example_input.txt");
 Run(@"..\..\..\real_input.txt");
 
 void Run(string inputfile)
@@ -13,9 +15,45 @@ void Run(string inputfile)
 
     for (int i = 0; i < S.Count; i++)
     {
-
+        var l = S[i].Length /2;
+        var L = S[i].ToArray();
+        var p1 = L.Skip(l).ToArray();
+        foreach (var c in L.Take(l))
+        {
+            if (p1.Contains(c))
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    answer1 += 27 + (c - 'A');
+                }
+                else
+                {
+                    answer1 += 1 + (c - 'a');
+                }
+                break;
+            }
+        }
     }
 
+    for (int i = 0; i < S.Count / 3; i += 3)
+    {
+        foreach (var c in S[i])
+        {
+            if (S[i+1].Contains(c) && S[i+2].Contains(c))
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    answer2 += 27 + (c - 'A');
+                }
+                else
+                {
+                    answer2 += 1 + (c - 'a');
+                }
+                break;
+            }
+         }
+
+    }
 
     w(1, answer1, supposedanswer1);
     w(2, answer2, supposedanswer2);
