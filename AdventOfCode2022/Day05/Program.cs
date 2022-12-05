ab9666913ab3f3582fea34db15e44c5cbfbf42d3
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-05 06:46:48 +0100

    Added Day05

--- AdventOfCode2022/Day05/Program.cs
+++ AdventOfCode2022/Day05/Program.cs
@@ -3,17 +3,76 @@ Run(@"..\..\..\real_input.txt", false);
 
 void Run(string inputfile, bool isTest)
 { 
-    long supposedanswer1 = 0000;
-    long supposedanswer2 = 0000;
+    string supposedanswer1 = "CMZ";
+    string supposedanswer2 = "MCD";
 
     var S = File.ReadAllLines(inputfile).ToList();
-    long answer1 = 0;
-    long answer2 = 0;
+    string answer1 = "";
+    string answer2 = "";
 
+    int i = 0;
+    var stacks = new Dictionary<int, Stack<char>>();
 
-    for (int i = 0; i < S.Count; i++)
+    while (S[i][1] != '1')
     {
-     
+        int z = 1;
+        for (int j = 1; j < S[i].Length; j += 4)
+        {
+            if (S[i][j] <= 'Z' && S[i][j] >= 'A')
+            {
+                Stack<char> s;
+                if (!stacks.TryGetValue(z, out s)) s = new Stack<char>();
+                s.Push(S[i][j]);
+                stacks[z] = s;
+            }
+            z++;
+        }
+        i++;
+    }
+    
+    var stc2 = new Dictionary<int,Stack<char>>();
+    foreach(var ss in stacks)
+    {
+        var s1 = new Stack<char>();
+        var s2 = new Stack<char>();
+        while (ss.Value.Count > 0)
+        {
+            var c = ss.Value.Pop();
+            s1.Push(c);
+            s2.Push(c);
+        }
+        stacks[ss.Key] = s1;
+        stc2[ss.Key] = s2;
+    }
+
+    i++; i++;
+    while (i < S.Count)
+    {
+        var s1 = S[i].Split(' ');
+        var count = int.Parse(s1[1]);
+        var frm = int.Parse(s1[3]);
+        var untl = int.Parse(s1[5]);
+        var s2 = new Stack<char>();
+        for (int z = 0; z < count; z++)
+        {
+            var c = stacks[frm].Pop();
+            stacks[untl].Push(c);
+        
+            var c2 = stc2[frm].Pop();
+            s2.Push(c2);
+        }
+        for (int z = 0; z < count; z++)
+        {
+            var c = s2.Pop();
+            stc2[untl].Push(c);
+        }
+
+        i++;
+    }
+    foreach (var p in stacks.Keys.OrderBy(k => k))
+    {
+        answer1 += stacks[p].Peek();
+        answer2 += stc2[p].Peek();
     }
 
     w(1, answer1, supposedanswer1, isTest);
