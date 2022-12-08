147eb5530b3cb947ddc402c45d9dc9d34197242d
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-08 07:03:48 +0100

    Prepared Day 10

---
+++ AdventOfCode2022/Day10/Program.cs
@@ -0,0 +1,41 @@
+﻿Run(@"..\..\..\example_input.txt", true);
+Run(@"..\..\..\real_input.txt", false);
+
+void Run(string inputfile, bool isTest)
+{ 
+    long supposedanswer1 = 0000;
+    long supposedanswer2 = 0000;
+
+    var S = File.ReadAllLines(inputfile).ToList();
+    long answer1 = 0;
+    long answer2 = 0;
+
+    for (int i = 0; i < S.Count; i++)
+    {
+     
+    }
+
+    w(1, answer1, supposedanswer1, isTest);
+    w(2, answer2, supposedanswer2, isTest);
+}
+
+static void w<T>(int number, T val, T supposedval, bool isTest)
+{
+    string? v = (val == null) ? "(null)" : val.ToString();
+    string? sv = (supposedval == null) ? "(null)" : supposedval.ToString();
+
+    var previouscolour = Console.ForegroundColor;
+    Console.Write("Answer Part " + number + ": ");
+    Console.ForegroundColor = (v == sv) ? ConsoleColor.Green : ConsoleColor.White;
+    Console.Write(v);
+    Console.ForegroundColor = previouscolour;
+    if (isTest)
+    {
+        Console.Write(" ... supposed (example) answer: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(sv);
+        Console.ForegroundColor = previouscolour;
+    }
+    else
+        Console.WriteLine();
+}
