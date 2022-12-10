dcff04bd7892989dc6ef3d0b3f2a66feec828189
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-10 05:55:08 +0100

    Added Day9 Part II

--- AdventOfCode2022/Day09/Program.cs
+++ AdventOfCode2022/Day09/Program.cs
@@ -12,14 +12,14 @@ void Run(string inputfile, bool isTest)
     var S = File.ReadAllLines(inputfile).ToList();
     long answer1 = 0;
     long answer2 = 0;
-    (int x, int y) H = (0, 0);
-    (int x, int y) T = (0, 0);
+    (int x, int y)[] rope1 = { (0, 0), (0, 0) };
     var visited = new HashSet<(int x, int y)>();
-    visited.Add(H);
+    visited.Add(rope1[0]);
 
+    var rope2 = new (int x, int y)[10];
     var visited2 = new HashSet<(int x, int y)>();
-    visited2.Add(H);
-    var rope = new (int x, int y)[10];
+    visited.Add(rope2[0]);
+
     for (int i = 0; i < S.Count; i++)
     {
         var cmd = S[i].Split(' ');
@@ -27,110 +27,52 @@ void Run(string inputfile, bool isTest)
         int step = int.Parse(cmd[1]);
         for (int j = 0; j < step; j++)
         {
-            (H, T) = Move(H, T, dir);
-            visited.Add(T);
-
-            for (int k= 0; k < 9; k++)
-            {
-                // <HIER VANMIDDAG VERDER >
-            }
+            Move(rope1, dir);
+            visited.Add(rope1[1]);
+            Move(rope2, dir);
+            visited2.Add(rope2[9]);
         }
     }
 
     answer1 = visited.Count();
+    answer2 = visited2.Count();
     w(1, answer1, supposedanswer1, isTest);
     w(2, answer2, supposedanswer2, isTest);
 }
 
-static ((int x, int y), (int x, int y)) Move ((int x, int y)H, (int x, int y)T, string dir)
+static void Move ((int x, int y)[] rope, string dir)
 {
-    int difx = H.x - T.x;
-    int dify = H.y - T.y;
-    bool U = dir == "U" ;
-    bool D = dir == "D";
-    bool R = dir == "R";
-    bool L = dir == "L";
+    (int x, int y) move = (0,0);
 
+    if (dir == "U") move = (0, 1);
+    else if (dir == "D") move = (0, -1);
+    else if (dir == "R") move = (1, 0);
+    else if (dir == "L") move = (-1, 0);
+    rope[0].x += move.x;
+    rope[0].y += move.y;
 
-    if ((difx, dify) == (-1, 1))
+    for (int i = 0; i < rope.Length - 1;  i++)
     {
-        if (L || U)
-        {
-            T.x--;
-            T.y++;
-        }
-    }
-    else if ((difx, dify) == (-1, 0))
-    {
-        if ( L )
-        {
-            T.x--;
-        }
-    }
-    else if ((difx, dify) == (-1, -1))
-    {
-        if ( L || D)
-        {
-            T.x--;
-            T.y--;
-        }
-    }
-    else if ((difx, dify) == (0, -1))
-    {
-        if ( D )
-        {
-            T.y--;
-        }
-    }
-    else if ((difx, dify) == (1, -1))
-    {
-        if ( D || R)
-        {
-            T.x++;
-            T.y--;
-        }
-    }
-    else if ((difx, dify) == (1, 0))
-    {
-        if ( R )
-        {
-            T.x++;
-        }
-    }
-    else if ((difx, dify) == (1, 1))
-    {
-        if (R || U)
-        {
-            T.x++;
-            T.y++;
-        }
-    }
-    else if ((difx, dify) == (0, 1))
-    {
-        if (  U )
-        {
-            T.y++;
-        }
-    }
+        (int x, int y) dif; 
+        dif.x = rope[i].x - rope[i+1].x;
+        dif.y = rope[i].y - rope[i+1].y;
+        
+        (int x, int y) newmove = (0,0);
 
-    if (R)
-    {
-        H.x++;
-    }
-    else if (L)
-    {
-        H.x--;
-    }
-    else if (U)
-    {
-        H.y++;
-    }
-    else if (D)
-    {
-        H.y--;
-    }
+        if (dif == (-2, 1) || dif == (-2, 2) || dif == (-1, 2)) newmove = (-1, 1);
+        else if (dif == (-2, 0)) newmove = (-1, 0);
+        else if (dif == (-2, -1) || dif == (-2, -2) || dif == (-1, -2)) newmove = (-1, -1);
+        else if (dif == (0, -2)) newmove = (0, -1);
+        else if (dif == (2, -1) || dif == (2, -2) || dif == (1, -2)) newmove = (1, -1);
+        else if (dif == (2, 0)) newmove = (1, 0);
+        else if (dif == (2, 1) || dif == (2, 2) || dif == (1, 2)) newmove = (1, 1);
+        else if (dif == (0, 2)) newmove = (0, 1);
 
-    return (H, T);
+        rope[i+1].x += newmove.x;
+        rope[i+1].y += newmove.y;
+
+    }
+    return;
 }
 
 static void w<T>(int number, T val, T supposedval, bool isTest)
