e722f1122ae9401530dc47bfb22887ddfc848044
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-13 12:07:22 +0100

    Added 2022 Day13

--- AdventOfCode2022/Day13/Program.cs
+++ AdventOfCode2022/Day13/Program.cs
@@ -1,21 +1,53 @@
-﻿Run(@"..\..\..\example_input.txt", true);
+﻿using System.Diagnostics;
+using System.Net.Security;
+
+Run(@"..\..\..\example_input.txt", true);
 Run(@"E:\develop\advent-of-code-input\2022\day13.txt", false);
 
 void Run(string inputfile, bool isTest)
-{ 
-    long supposedanswer1 = 0000;
-    long supposedanswer2 = 0000;
-
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    long supposedanswer1 = 13;
+    long supposedanswer2 = 140;
+    
     var S = File.ReadAllLines(inputfile).ToList();
     long answer1 = 0;
     long answer2 = 0;
 
-    for (int i = 0; i < S.Count; i++)
+    var packages = new List<string>();
+    string p1 = "[[2]]";
+    string p2 = "[[6]]";
+    packages.Add(p1);
+    packages.Add(p2);
+
+    int i = 0;
+    int idx = 1;
+    while (i<S.Count)
     {
-     
+        var left = S[i];
+        var right = S[i + 1];
+        int order = cmp.compare1(new string[] { left }, new string[] { right });
+        if (order == -1) answer1 += idx;
+        packages.Add(left);
+        packages.Add(right);
+        i += 3;
+        idx++;
     }
 
+    stopwatch.Stop();
+    Console.WriteLine($"Used time (ms): {stopwatch.ElapsedMilliseconds}");
+    Console.WriteLine($"Used time (ticks): {stopwatch.ElapsedTicks}");
     w(1, answer1, supposedanswer1, isTest);
+
+    stopwatch.Restart();
+    packages.Sort(new cmp());
+    int l1 = packages.IndexOf(p1) + 1;
+    int l2 = packages.IndexOf(p2) + 1;
+    answer2 = l1 * l2;
+
+    stopwatch.Stop();
+    Console.WriteLine($"Used time (ms): {stopwatch.ElapsedMilliseconds}");
+    Console.WriteLine($"Used time (ticks): {stopwatch.ElapsedTicks}");
     w(2, answer2, supposedanswer2, isTest);
 }
 
@@ -38,4 +70,80 @@ static void w<T>(int number, T val, T supposedval, bool isTest)
     }
     else
         Console.WriteLine();
+}
+
+class cmp : IComparer<string>
+{
+    int IComparer<string>.Compare(string? x, string? y)
+    {
+        return compare1(new string[] { x }, new string[] { y });
+        
+    }
+    public static int compare1(string[] left, string[] right)
+    {
+        for (int i = 0; i < left.Length; i++)
+        {
+            if (right.Length <= i) return 1;
+
+            if (left[i].Length == 0) return right[i].Length == 0 ? 0 : -1;
+            else if (right[i].Length == 0) return 1;
+
+            if (left[i][0] == '[')
+            {
+                var l1 = makelist(left[i].Substring(1, left[i].Length - 2));
+                if (right[i][0] == '[')
+                {
+                    var r1 = makelist(right[i].Substring(1, right[i].Length - 2));
+                    int order = compare1(l1, r1);
+                    if (order != 0) return order;
+                }
+                else
+                {
+                    var r1 = new string[] { right[i] };
+                    int order = compare1(l1, r1);
+                    if (order != 0) return order;
+                }
+            }
+            else if (right[i][0] == '[')
+            {
+                var l1 = new string[] { left[i] };
+                var r1 = makelist(right[i].Substring(1, right[i].Length - 2));
+                int order = compare1(l1, r1);
+                if (order != 0) return order;
+            }
+            else
+            {
+                int a = int.Parse(left[i]);
+                int b = int.Parse(right[i]);
+                if (a > b) return 1;
+                else if (a < b) return -1;
+            }
+        }
+        if (right.Length > left.Length) return -1;
+        return 0;
+    }
+
+    static string[] makelist(string s)
+    {
+        var l = new List<string>();
+        var s1 = "";
+        int level = 0;
+        foreach (var c in s)
+        {
+            if (c == '[') { level++; s1 += c; }
+            else if (c == ']') { level--; s1 += c; }
+            else if (c == ',')
+            {
+                if (level == 0)
+                {
+                    l.Add(s1);
+                    s1 = "";
+                }
+                else s1 += c;
+            }
+            else s1 += c;
+        }
+        l.Add(s1);
+        return l.ToArray();
+    }
 }
