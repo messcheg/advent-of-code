d7fad847216ec7752e9f54cc1f51070e19f34502
Gert-Jan Messchendorp <messcheg@gmail.com>
2022-12-20 11:22:14 +0100

    Added 2022 Day 20

---
+++ AdventOfCode2022/Day22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Net.Security;
+
+Run(@"..\..\..\example_input.txt", true);
+Run(@"E:\develop\advent-of-code-input\2022\day22.txt", false);
+
+void Run(string inputfile, bool isTest)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    long supposedanswer1 = 13;
+    long supposedanswer2 = 140;
+    
+    var S = File.ReadAllLines(inputfile).ToList();
+    long answer1 = 0;
+    long answer2 = 0;
+
+  
+    int i = 0;
+    while (i<S.Count)
+    {
+        var s = S[i];
+
+
+        i++;
+    }
+
+    stopwatch.Stop();
+    Console.WriteLine($"Used time (ms): {stopwatch.ElapsedMilliseconds}");
+    Console.WriteLine($"Used time (ticks): {stopwatch.ElapsedTicks}");
+    w(1, answer1, supposedanswer1, isTest);
+    w(2, answer2, supposedanswer2, isTest);
+}
+
+static void w<T>(int number, T val, T supposedval, bool isTest)
+{
+    string? v = (val == null) ? "(null)" : val.ToString();
+    string? sv = (supposedval == null) ? "(null)" : supposedval.ToString();
+
+    var previouscolour = Console.ForegroundColor;
+    Console.Write("Answer Part " + number + ": ");
+    Console.ForegroundColor = (v == sv) ? ConsoleColor.Green : ConsoleColor.White;
+    Console.Write(v);
+    Console.ForegroundColor = previouscolour;
+    if (isTest)
+    {
+        Console.Write(" ... supposed (example) answer: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(sv);
+        Console.ForegroundColor = previouscolour;
+    }
+    else
+        Console.WriteLine();
+}
