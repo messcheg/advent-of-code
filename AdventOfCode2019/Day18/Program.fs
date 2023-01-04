851e433c847d78c64abc0cb4e9c60f91932526e8
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-01-04 23:04:05 +0100

    Added 'Day31' a classical TSP problem to solve a real problem of one of my friends.

--- AdventOfCode2019/Day18/Program.fs
+++ AdventOfCode2019/Day18/Program.fs
@@ -17,24 +17,45 @@ let rec startlocation i j =
         elif i+1 < lenI then startlocation (i+1) 0
         else (-1,-1)
 
-let rec allKeys startlocation i j = 
+let rec allKeys i j = 
     let lenI = Array.length inp
     let lenJ = String.length inp[0]
-    let own = if inp[i][j] in {'a'..'z'} then {inp[i][j]} else {}
+    let next =
+        if j+1 < lenJ then allKeys i (j+1)
+        elif i+1 < lenI then allKeys (i+1) 0
+        else Set.empty
+    if inp[i][j] >= 'a' && inp[i][j] <= 'z' then next.Add(inp[i][j]) else next
 
-        if j+1 < lenJ then startlocation i (j+1)
-        elif i+1 < lenI then startlocation (i+1) 0
-        else (-1,-1)
-
-let reachable i j keys cost =
+let GetAllKeys = allKeys 0 0
+let GetStartLocation = startlocation 0 0
+ 
+let rec reachable i j (collectedKeys: Set<char>) (visited: Set<int * int>) costs =
     let lenI = Array.length inp
     let lenJ = String.length inp[0]
-    if inp[i][j] = '.' then
+    if inp[i][j] = '#' then Set.empty
+    elif inp[i][j] >= 'A' && inp[i][j] <= 'Z' && not (collectedKeys.Contains( inp[i][j] + 'z' - 'a' )) then Set.empty 
+    elif visited.Contains((i,j)) then Set.empty
+    else    
+        let newVisit = Set.empty.Add((i,j)) + visited
+        let newCosts = costs + 1
+        let n1 = if i + 1 < lenI then reachable (i+1) j collectedKeys newVisit newCosts else Set.empty 
+        let n2 = if i > 0 then reachable (i-1) j collectedKeys newVisit newCosts  else Set.empty 
+        let n3 = if j + 1 < lenJ then reachable i (j+1) collectedKeys newVisit newCosts  else Set.empty 
+        let n4 = if j > 0 then reachable i (j-1) collectedKeys newVisit newCosts  else Set.empty 
+        let nAll = n1 + n2 + n3 + n4
+        if inp[i][j] >= 'a' && inp[i][j] <= 'z' && not (collectedKeys.Contains( inp[i][j])) then 
+            nAll.Add((inp[i][j],(i , j), costs)) 
+        else nAll
+        
+let GetReachable i j collectedKeys = reachable i j collectedKeys Set.empty 0
         
 
-
 let rec findpath worklist visited =
-    let next = List.head worklist
+    let next = List.sortBy (fun (cost, path) -> cost) worklist 
+    let h = List.head next
+    let t = List.tail next
+
+
 
     // mijn idee voor de oplossing: set-met-sleutels-leeg is "klaar"
     // sleutel pakken is uit-de-set kortstepad naar de sleutel
--- AdventOfCode2022/Day31Bonus/Program.cs
+++ AdventOfCode2022/Day31Bonus/Program.cs
@@ -8,119 +8,163 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Run(@"..\..\..\example_input.txt", true);
-        // Run(@"..\..\..\real_input.txt", false);
+        //Run(@"..\..\..\example_input.txt", false);
+        //Run(@"..\..\..\real_input.txt", false);
+        Run(@"..\..\..\DistanceGraph.txt", true);
 
 
-        void Run(string inputfile, bool isTest)
+        void Run(string inputfile, bool isMatrix)
         {
+            Console.WriteLine($"--------------[{inputfile}]--------------------");
             Stopwatch stopwatch = Stopwatch.StartNew();
             var postcodefile = @"..\..\..\czech_cities.txt";
 
             var S = File.ReadAllLines(inputfile).ToList();
             var postcodes = File.ReadAllLines(postcodefile).Skip(1).Select(s => s.Split(',')).ToList();
-            long answer1 = 0;
-            long supposedanswer1 = 0;
+            List<(string name, string postal, GeoCoordinate location)> cities;
+            double[,] distances;
+            if(!isMatrix)
+                LoadFromCityfile(S, postcodes, out cities, out distances);
+            else
+                LoadFromHencofile(S, postcodes, out cities, out distances);
 
-            var cities = new List<(string name, string postal, GeoCoordinate location)>();
-            // calculate all distances
-            var distances = new double[S.Count, S.Count];
+            List<(double distance, double hash, int[] path)>? best = GetBestpath(cities, distances);
 
-            int i = 0;
-            while (i < S.Count)
+            for (int j = 0; j < best[0].path.Length; j++)
             {
-                var s = S[i];
-                var detail = postcodes.Where(a => a[2].StartsWith(s)).ToList();
-                Debug.Assert(detail.Count > 0);
-                double latitude = double.Parse(detail[0][9],CultureInfo.InvariantCulture);
-                double longitude = double.Parse(detail[0][10], CultureInfo.InvariantCulture);
-
-                cities.Add((s, detail[0][1], new GeoCoordinate(latitude , longitude )));
-
-                for (int j = 0; j < cities.Count; j++) 
+                if (j > 0)
                 {
-                    if (j != i)
-                    {
-                        var distance = cities[j].location.GetDistanceTo(cities[i].location);
-                        distances[i, j] = distances[j, i] = distance;
-                    }
+                    Console.WriteLine("         |  ");
+                    Console.WriteLine($" drive: {distances[best[0].path[j - 1], best[0].path[j]]} {(isMatrix ? "km" : "m")}");
+                    Console.WriteLine("         |  ");
                 }
-                i++;
-            }
-            double TotalDistance(int[] x)
-            {
-                double result = 0;
-                for (int p = 0; p < x.Length - 1; p++)
-                {
-                    result += distances[x[p], x[p + 1]];
-                }
-                return result;
-            }
-            // start searching
-            var best = new List<int[]>();
-            best.Add(new int[cities.Count + 1]);
-            for (int j= 0; j < cities.Count; j++)
-            {
-                best[0][j] = j;
-            }
-            var rnd = new Random();
-            for (int k = 0; k < 100; k++)
-            {
-                var neighbours = new List<int[]>();
-                
-                foreach (var b in best)
-                {
-                    neighbours.Add(b);
-                    for (int m = 0; m< 10; m++)
-                    {
-                        var nb = b.ToArray();
-                        int cityFrom = rnd.Next(1,cities.Count-1);
-                        int cityTo = rnd.Next(1, cities.Count - 1);
-                        if (cityFrom != cityTo)
-                        {
-                            (nb[cityFrom], nb[cityTo]) = (nb[cityTo], nb[cityFrom]);
-                            neighbours.Add(nb);
-                        }
-                    }
-                }
-                
-                best = neighbours.Distinct().OrderBy(x => TotalDistance(x)).Take(10).ToList();
-            }
+                var bc = cities[best[0].path[j]];
+                Console.WriteLine($"{bc.name} postcode:{bc.postal} ({bc.location.ToString()})");
 
-            for (int j=0; j< best[0].Length; j++) 
-            {
-                var bc = cities[best[0][j]];
-                Console.WriteLine($"{bc.name} , {bc.location.ToString()}");
-            }
-            Console.WriteLine($"Total distance: {TotalDistance(best[0])}, (745282,1772587214)");
+             }
+            Console.WriteLine($"Total distance: {best[0].distance} {(isMatrix ? "km": "m")}");
 
             stopwatch.Stop();
-            Console.WriteLine($"Used time (ms): {stopwatch.ElapsedMilliseconds}");
-            Console.WriteLine($"Used time (ticks): {stopwatch.ElapsedTicks}");
-            w(1, answer1, supposedanswer1, isTest);
-        }
-
-
-        static void w<T>(int number, T val, T supposedval, bool isTest)
-        {
-            string? v = val == null ? "(null)" : val.ToString();
-            string? sv = supposedval == null ? "(null)" : supposedval.ToString();
-
-            var previouscolour = Console.ForegroundColor;
-            Console.Write("Answer Part " + number + ": ");
-            Console.ForegroundColor = v == sv ? ConsoleColor.Green : ConsoleColor.White;
-            Console.Write(v);
-            Console.ForegroundColor = previouscolour;
-            if (isTest)
-            {
-                Console.Write(" ... supposed (example) answer: ");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(sv);
-                Console.ForegroundColor = previouscolour;
-            }
-            else
-                Console.WriteLine();
+            Console.WriteLine($"Used time to caclulate (ms): {stopwatch.ElapsedMilliseconds}");
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
         }
     }
 
+    private static void LoadFromHencofile(List<string> S, List<string[]> postcodes, out List<(string name, string postal, GeoCoordinate location)> cities, out double[,] distances)
+    {
+        cities = new List<(string name, string postal, GeoCoordinate location)>();
+        // calculate all distances
+        distances = new double[S.Count, S.Count];
+        int i = 0;
+        while (i+3 < S.Count)
+        {
+            var s = S[i+3].Split('\t');
+            if (i == 0) cities.Add((s[0], s[2], new GeoCoordinate(52.018223, 5.156038)));
+            else
+            {
+                var detail = postcodes.Where(a => a[1] == s[2][0..3] + " " + s[2][3..5]).ToList();
+                Debug.Assert(detail.Count > 0);
+                double latitude = double.Parse(detail[0][9], CultureInfo.InvariantCulture);
+                double longitude = double.Parse(detail[0][10], CultureInfo.InvariantCulture);
+
+                cities.Add((s[0], detail[0][1], new GeoCoordinate(latitude, longitude)));
+            }
+            for (int j = 0; j < cities.Count; j++)
+            {
+                if (j != i)
+                {
+                    var distance = double.Parse(s[j + 3]);
+                    distances[i, j] = distances[j, i] = distance;
+                }
+            }
+            i++;
+        }
+    }
+
+    private static void LoadFromCityfile(List<string> S, List<string[]> postcodes, out List<(string name, string postal, GeoCoordinate location)> cities, out double[,] distances)
+    {
+        cities = new List<(string name, string postal, GeoCoordinate location)>();
+        // calculate all distances
+        distances = new double[S.Count, S.Count];
+        int i = 0;
+        while (i < S.Count)
+        {
+            var s = S[i];
+            var detail = postcodes.Where(a => a[2].StartsWith(s)).ToList();
+            Debug.Assert(detail.Count > 0);
+            double latitude = double.Parse(detail[0][9], CultureInfo.InvariantCulture);
+            double longitude = double.Parse(detail[0][10], CultureInfo.InvariantCulture);
+
+            cities.Add((s, detail[0][1], new GeoCoordinate(latitude, longitude)));
+
+            for (int j = 0; j < cities.Count; j++)
+            {
+                if (j != i)
+                {
+                    var distance = cities[j].location.GetDistanceTo(cities[i].location);
+                    distances[i, j] = distances[j, i] = distance;
+                }
+            }
+            i++;
+        }
+    }
+
+    private static List<(double distance, double hash, int[] path)> GetBestpath(List<(string name, string postal, GeoCoordinate location)>? cities, double[,] distances)
+    {
+        double TotalDistance(int[] x)
+        {
+            double result = 0;
+            for (int p = 0; p < x.Length - 1; p++)
+            {
+                result += distances[x[p], x[p + 1]];
+            }
+            return result;
+        }
+        double GetHash(int[] x)
+        {
+            double result = x[1];
+            for (int p = 2; p < x.Length - 1; p++)
+            {
+                result += result * x.Length + x[p];
+            }
+            return result;
+        }
+
+        // start searching
+        var best = new List<(double distance, double hash, int[] path)>();
+
+        best.Add((0, 0, new int[cities.Count + 1]));
+        for (int j = 0; j < cities.Count; j++)
+        {
+            best[0].path[j] = j;
+        }
+        best[0] = (TotalDistance(best[0].path), GetHash(best[0].path), best[0].path);
+        var rnd = new Random();
+        for (int k = 0; k < 100; k++)
+        {
+            var neighbours = new List<(double distance, double hash, int[] path)>();
+
+            foreach (var b in best)
+            {
+                neighbours.Add(b);
+                for (int m = 0; m < 10; m++)
+                {
+                    var nb = b.path.ToArray();
+                    int cityFrom = rnd.Next(1, cities.Count - 1);
+                    int cityTo = rnd.Next(1, cities.Count - 1);
+                    if (cityFrom != cityTo)
+                    {
+                        (nb[cityFrom], nb[cityTo]) = (nb[cityTo], nb[cityFrom]);
+                        neighbours.Add((TotalDistance(nb), GetHash(nb), nb));
+                    }
+                }
+            }
+
+            var b1 = neighbours.DistinctBy(x => (x.distance, x.hash));
+            best = b1.OrderBy(x => x.distance).Take(10).ToList();
+        }
+
+        return best;
+    }
 }
