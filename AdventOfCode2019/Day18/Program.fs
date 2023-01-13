6f18d03114c4d1807b458d4825d78ad6f5c44b48
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-01-13 08:29:53 +0100

    Added 2019 Day 18 part 1

--- AdventOfCode2019/Day18/Program.fs
+++ AdventOfCode2019/Day18/Program.fs
@@ -2,24 +2,28 @@
 
 let ex1 = "..\\..\\..\\Example1.txt"
 let ex2 = "..\\..\\..\\Example2.txt"
+let ex3 = "..\\..\\..\\Example3.txt"
 let rl = "..\\..\\..\\real_input.txt"
 
 let inp = 
-    File.ReadLines ex2 |>
+    File.ReadLines rl |>
     Seq.toArray
 
-let rec allKeys i j (allkeys:Map<char,int*int>) (alldoors:Map<char,int*int>)= 
+let rec allKeys i j (allkeys:Map<char,int*int>) (alldoors:Map<char,int*int>) (everything:Map<char,int*int>)= 
     let lenI = Array.length inp
     let lenJ = String.length inp[0]
     let (nextI, nextJ) =
         if j+1 < lenJ then (i, (j+1))
         else ((i+1), 0)
-    if i >= lenI then (allkeys, alldoors)
-    elif ( inp[i][j] >= 'a' && inp[i][j] <= 'z') || inp[i][j] = '@' then allKeys nextI nextJ (allkeys.Add(inp[i][j], (i,j))) alldoors 
-    elif ( inp[i][j] >= 'A' && inp[i][j] <= 'Z') then allKeys nextI nextJ allkeys (alldoors.Add(inp[i][j], (i,j))) 
-    else  allKeys nextI nextJ allkeys alldoors
+    if i >= lenI then (allkeys, alldoors, everything)
+    else
+        let k = inp[i][j]
+        let p = (i,j)
+        let (ak1, ev1) = if (k >= 'a' && k <= 'z') || k = '@' then (allkeys.Add(k,p), everything.Add(k,p)) else (allkeys,everything)
+        let (ad1, ev2) = if (k >= 'A' && k <= 'Z') then (alldoors.Add(k,p), ev1.Add(k,p)) else (alldoors,ev1)
+        allKeys nextI nextJ ak1 ad1 ev2
 
-let GetAllKeys = allKeys 0 0 Map.empty Map.empty
+let GetAllKeys = allKeys 0 0 Map.empty Map.empty Map.empty
  
 let rec reachable (loc:int*int) (visited: Set<int * int>) (from:char) (reaching:Map<char,Set<char>>)=
     let lenI = Array.length inp
@@ -28,23 +32,21 @@ let rec reachable (loc:int*int) (visited: Set<int * int>) (from:char) (reaching:
     let k = inp[i][j]
     if k = '#' then reaching
     elif visited.Contains((i,j)) then reaching
+    elif (k <> from) && ((k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z' )) then
+        let n4 = reaching
+        if n4.ContainsKey(from) then 
+            if not (n4[from].Contains(k)) then
+                let newC = n4[from].Remove(k).Add(k)
+                n4.Remove(from).Add(from, newC)
+            else n4
+        else n4.Add(from,Set.empty.Add(k))
     else
-        let newFrom = if (k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z' ) then k else from 
-    
         let newVisit = visited.Add((i,j))
-        let n1 = if i + 1 < lenI then reachable ((i+1), j) newVisit newFrom reaching else reaching
-        let n2 = if i > 0 then reachable ((i-1), j) newVisit newFrom n1  else n1 
-        let n3 = if j + 1 < lenJ then reachable (i, (j+1)) newVisit newFrom n2  else n2 
-        let n4 = if j > 0 then reachable (i, (j-1)) newVisit newFrom n3 else n3
-        
-        if (k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z' ) then
-            if n4.ContainsKey(from) then 
-                if not (n4[from].Contains(k)) then
-                    let newC = n4[from].Remove(k).Add(k)
-                    n4.Remove(from).Add(from, newC)
-                else n4
-            else n4.Add(from,Set.empty.Add(k))
-        else n4
+        let n1 = if i + 1 < lenI then reachable ((i+1), j) newVisit from reaching else reaching
+        let n2 = if i > 0 then reachable ((i-1), j) newVisit from n1  else n1 
+        let n3 = if j + 1 < lenJ then reachable (i, (j+1)) newVisit from n2  else n2 
+        if j > 0 then reachable (i, (j-1)) newVisit from n3 else n3
+                
 
 let rec mapKeys (loc:int*int) (visited: Set<int * int>) (costs:int) (from:char) (map:Map<char*char,int>)=
     let lenI = Array.length inp
@@ -70,9 +72,15 @@ let rec mapKeys (loc:int*int) (visited: Set<int * int>) (costs:int) (from:char) 
         let n4 = if j > 0 then mapKeys (i, (j-1)) newVisit newCosts from n3 else n3
         n4
         
-
+let rec reachableSet (allkeys:Map<char,int*int>) (reaching:Map<char,Set<char>>) =
+    if allkeys.Count = 0 then reaching
+    else
+        let k = allkeys.Keys |> Seq.head
+        let pos = allkeys[k]
+        let newReach = reachable pos Set.empty k reaching
+        reachableSet (allkeys.Remove(k)) newReach
         
-let GetReachable (allkeys:Map<char,int*int>) = reachable allkeys['@'] Set.empty '@' (Map.empty.Add('@', Set.empty))
+let GetReachable (allkeys:Map<char,int*int>) = reachableSet allkeys Map.empty
 
 let rec distanceMapToKey i j k (keys:Map<char,int*int>) (map:Map<char*char,int>) =
     mapKeys (i,j) Set.empty 0 k map
@@ -90,77 +98,75 @@ let rec distanceMap (keys:Map<char,int*int>) (map:Map<char*char,int>) =
         
 let GetDistanceMap (keys:Map<char,int*int>) = distanceMap keys Map.empty
 
-let (mk,md) = GetAllKeys
-let KeyTree = GetReachable mk
+let (mk,md,me) = GetAllKeys
+let KeyMap = GetReachable me 
 let KeyDistMap = GetDistanceMap mk
 
-printfn "Check: %d" KeyTree.Keys.Count
+printfn "Check: %d" KeyMap.Keys.Count
 printfn "Check: %d" KeyDistMap.Keys.Count
 
-let rec husslekey (h1:string) (h2:string) (cnt:int) (result:List<string*string>) = 
-    if cnt = h2.Length then
-        result
-    else
-        let k =  h2[cnt]
-        if 'A' <= k && k <= 'Z' && h2.Contains(k + 'a' - 'A') then
-            husslekey h1 h2 (cnt+1) result
-        else
-            husslekey h1 h2 (cnt+1) (result @ [(h1 + h2[cnt..cnt], h2[0..cnt-1] + h2[cnt+1..])])
+let lower k = 
+    if 'A' <= k && k <= 'Z' then k + 'a' - 'A'
+    else k
 
-let rec doHussle (collected:List<string*string>)=
-    let (h1,h2) = collected[0]
-    if h2.Length = 0 then 
-        collected
-    else
-        doHussle (collected[1..] @ (husslekey h1 h2 0 List.empty) ) 
+let validkey (k:char) (collectedKeys:Set<char>) =
+    let a = mk.ContainsKey(k)
+    let b = (not (collectedKeys.Contains(lower(k))))
+    a && b
 
-let rec Usefull (s:string) startloc =
-    let firstDoorIdx = s.IndexOfAny((KeyTree.Keys |> Seq.toArray), startloc)
-    if firstDoorIdx = -1 then true
-    else
-        let k = s[firstDoorIdx] - 'A' + 'a'
-        let pos =  s.IndexOf(k)
-        if pos > firstDoorIdx then false
-        elif pos = -1 then 
-            if startloc = 0 then false
-            else true
-        else Usefull s (firstDoorIdx+1)
+let validdoor (k:char) (collectedKeys:Set<char>) (deeper:Set<char>) =
+    let a = collectedKeys.Contains(lower k)
+    let b = not (deeper.Contains(k))
+    a && b
 
-let rec FilterUseless (col:List<string*string>) (res:List<string>) =
-    if col.Length = 0 then res
-    else
-        let (h,_) = col[0]
-        if Usefull h  0 then FilterUseless (col[1..]) (res @ [h]) 
-        else FilterUseless (col[1..]) res 
+let rec GetNextSteps (key:char) (collectedKeys:Set<char>) (deeper:Set<char>) =
+    let kmk = KeyMap[key]
+    let direct = kmk |> Seq.filter(fun k -> validkey k collectedKeys)
+    let ports = kmk |> Seq.filter(fun k -> validdoor k collectedKeys deeper)
+    let sub = ports |> Seq.map(fun k -> GetNextSteps k collectedKeys (kmk + deeper))  
+    let sub1 = sub |> Seq.concat
+    direct|> Seq.append(sub1) |> Set.ofSeq
 
-let substituteAndHussleString (path:string) =
-    let firstDoorIdx = path.IndexOfAny(KeyTree.Keys |> Seq.toArray)
-    if firstDoorIdx = -1 then List.empty
-    else
-        let firstDoor = path[firstDoorIdx]
-        let reachable = KeyTree[firstDoor]
-        let part1 = path[0..firstDoorIdx-1]
-        let part2 = path[firstDoorIdx+1..] + (reachable |> Seq.toArray |> System.String)
-        let col = doHussle [(part1,part2)]
-        FilterUseless col List.empty
-
-let rec substituteAndHussle (paths:List<string>) =
-    let h = paths[0]
-    let subs = substituteAndHussleString h
-    if subs.IsEmpty then paths
+let rec AddWork (lst:List<char>) (workset:Set<int*char*string>) (discovered:Map<char*string, int*string>) (visited:Set<char*string>) (fromKey:char*string) (fromVal:int*string) =
+    if lst.Length = 0 then (workset, discovered)
     else 
-        substituteAndHussle (paths[1..] @ subs)
-(*
-let husseldedlist = substituteAndHussle ["@"]
+        let (cnt, pth) = fromVal  
+        let (key, collect) = fromKey
+        let addK = lst.Head
+        let nxt = lst.Tail
+        let nCnt = cnt + KeyDistMap[(key, addK)]
+        let nPth = ((pth |> Seq.toList) @ [addK] ) |> List.toArray  
+        let nCol = nPth |> Array.sort |> System.String
+        let nPthc = nPth |> System.String
+        let nKey = (addK,nCol)
+        if visited.Contains(nKey) then AddWork nxt workset discovered visited fromKey fromVal
+        elif discovered.ContainsKey(nKey) then  
+            let (dCnt, dpth) = discovered[nKey]
+            let nDisc = if dCnt > nCnt then discovered.Remove(nKey).Add(nKey,(nCnt,nPthc)) else discovered
+            let nWset = if dCnt > nCnt then workset.Remove((dCnt,addK,nCol)).Add((nCnt,addK,nCol)) else workset
+            AddWork nxt nWset nDisc visited fromKey fromVal
+        else 
+            AddWork nxt (workset.Add((nCnt,addK,nCol))) (discovered.Add(nKey,(nCnt,nPthc))) visited fromKey fromVal
+           
 
-printfn "Check list: %d"  husseldedlist .Length
-*)
+let rec shortest (workset:Set<int*char*string>) (discovered:Map<char*string, int*string>) (visited:Set<char*string>) = 
+    let work = workset |> Seq.head
+    let (count, curkey, collectedkeys) = work
+    let dKey = (curkey, collectedkeys)
+    if collectedkeys.Length = mk.Count - 1 then discovered[dKey]
+    else
+        let newV = visited.Add(dKey)
+        let dVal = discovered[dKey]
+        let newD = discovered.Remove(dKey)
+        let newW = workset.Remove(work)
 
-let rec shortest (work:Map<int, Map<int,Set<string>>>) = 
-    let cost = work.Keys |> Seq.head
-    let bestones = work[cost]
-    let longest = bestones.Keys |> Seq.last
-    let str = bestones[longest] |> Seq.head
+        let nxt = GetNextSteps curkey (collectedkeys |> Seq.toArray |> Set.ofArray)  Set.empty |> Seq.toList
+        let (newWorkSet, newDiscovered) = AddWork nxt newW newD newV dKey dVal 
+        shortest newWorkSet newDiscovered newV  
+  
+let sp = 
+    shortest (Set.empty.Add((0,'@' ,""))) (Map.empty.Add(('@',""),(0,""))) Set.empty
 
-    (0,"")
-    
+let (spCnt, spP) = sp 
+printfn "path %d" spCnt
+printfn "%s" spP
