9142a4e2f2475028f2bde40aa4c26bf4fa7449cc
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-04-27 00:55:56 +0200

    Prepared 2019 day25

---
+++ AdventOfCode2019/Day25/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+open IntcodeMachine.Intcode
+
+let filename = "E:\\develop\\advent-of-code-input\\2019\\Day25.txt"
+
+let prog1 = IntcodeMachine.Intcode.inp filename
+
+
+let rec runsteps pst cnt = 
+    if cnt > 0 then
+        let pst1 = runNextStepArr [||] pst
+        printf "%s" ((char (fst (fst pst1))).ToString())
+        runsteps (snd pst1) (cnt-1)
+    else
+        true
+
+runsteps (initalprogstate (Array.copy prog1)) 300 |> ignore
