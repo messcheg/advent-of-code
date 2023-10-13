a0635152535d6ed710a6a6577520040284e43ba5
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-10-13 23:58:51 +0200

    Added Day09 Part II

--- AdventOfCode2018/Day09/Program.vb
+++ AdventOfCode2018/Day09/Program.vb
@@ -2,42 +2,49 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        ' 479 players; l10st marble is worth 71035 points
-        Const players = 479
-        Const lastMarble = 7103500
+        Const players = 9
+        Const lastMarble = 25
 
         Dim scores() = New ULong(players) {}
-        Dim marbles As New List(Of Long)()
+        Dim marbles As New LinkedList(Of Long)()
 
-        Dim currentMarble = 0
         Dim currentplayer = 1
-        marbles.Add(0)
+        marbles.AddFirst(0)
+        Dim current = marbles.First
         For i = 1 To lastMarble
             If i Mod 23 = 0 Then
-                scores(currentplayer) += i
-                Dim marbleToTake = currentMarble - 7
-                While marbleToTake < 0
-                    marbleToTake += marbles.Count
-                End While
-                scores(currentplayer) += marbles(marbleToTake)
-                marbles.RemoveAt(marbleToTake)
-                currentMarble = marbleToTake
-                If currentMarble < 0 Then currentMarble += marbles.Count
+                Dim last = current
+                For k = 1 To 7
+                    last = current
+                    If current Is marbles.First Then
+                        current = marbles.Last
+                    Else
+                        current = current.Previous
+                    End If
+                Next
+                scores(currentplayer) += i + current.Value
+                marbles.Remove(current)
+                current = last
             Else
-                Dim insertAfter = currentMarble + 1
-                If insertAfter = marbles.Count Then insertAfter = 0
-                marbles.Insert(insertAfter + 1, i)
-                currentMarble = insertAfter + 1
+                If current Is marbles.Last Then
+                    current = marbles.First
+                Else
+                    current = current.Next
+                End If
+                current = marbles.AddAfter(current, i)
             End If
             'Console.Write("[" & currentplayer & "] ")
-            'For j = 0 To marbles.Count - 1
-            '    If j = currentMarble Then
-            '        Console.Write(" (" & marbles(j) & ") ")
+            'Dim mrb = marbles.First
+            'While Not mrb Is Nothing
+            '    If mrb Is current Then
+            '        Console.Write(" (" & mrb.Value & ") ")
             '    Else
-            '        Console.Write(" " & marbles(j) & " ")
+            '        Console.Write(" " & mrb.Value & " ")
             '    End If
-            'Next
+            '    mrb = mrb.Next
+            'End While
             'Console.WriteLine()
+
             currentplayer += 1
             If currentplayer > players Then currentplayer = 1
         Next
