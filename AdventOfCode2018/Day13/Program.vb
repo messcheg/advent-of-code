a8314c9d24520edcce92e3dbc786ca62aabcb726
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-10-18 06:32:04 +0200

    Added 2018 Day13 part II

--- AdventOfCode2018/Day13/Program.vb
+++ AdventOfCode2018/Day13/Program.vb
@@ -42,19 +42,10 @@ Module Program
         Dim seconds As Integer = 0
         Dim crashed As Boolean = False
         Dim crashCart As Cart
-        Dim prevlocations As New Dictionary(Of Tuple(Of Integer, Integer), List(Of Cart))
         While carts.Count > 1
-            Dim locations As New Dictionary(Of Tuple(Of Integer, Integer), List(Of Cart))
-            Dim remove As New HashSet(Of Tuple(Of Integer, Integer))
-            Dim newcarts As New List(Of Cart)
-            For Each crt1 In carts
-                Dim crt As New Cart
-                crt.id = crt1.id
-                crt.x = crt1.x
-                crt.y = crt1.y
-                crt.direction = crt1.direction
-                crt.nextAction = crt1.nextAction
-
+            Dim from_to As New List(Of Tuple(Of Integer, Tuple(Of Integer, Integer), Tuple(Of Integer, Integer)))
+            For Each crt In carts
+                Dim from As New Tuple(Of Integer, Integer)(crt.x, crt.y)
                 Dim curtrack As Char = map(crt.y)(crt.x)
                 Select Case curtrack
                     Case "|"
@@ -145,44 +136,55 @@ Module Program
                                 End Select
                         End Select
                 End Select
-                Dim tpl As New Tuple(Of Integer, Integer)(crt.x, crt.y)
-                If locations.ContainsKey(tpl) Then
-                    locations(tpl).Add(crt)
-                    remove.Add(tpl)
-                    If Not crashed Then crashCart = crt
-                    crashed = True
-                    remove.Add(tpl)
-                ElseIf prevlocations.ContainsKey(tpl) Then
-                    Dim prevcart = prevlocations(tpl)(0)
-                    locations(tpl).Add(crt)
-                    If Not crashed Then crashCart = crt
-                    crashed = True
-                    remove.Add(tpl)
+                Dim tpl_to As New Tuple(Of Integer, Integer)(crt.x, crt.y)
+                from_to.Add(New Tuple(Of Integer, Tuple(Of Integer, Integer), Tuple(Of Integer, Integer))(crt.id, from, tpl_to))
+            Next
+            'all same 'to'-fields need to be removed
+            Dim removeloc As New HashSet(Of Tuple(Of Integer, Integer))
+            Dim occupied As New HashSet(Of Tuple(Of Integer, Integer))
+            For Each ft In from_to
+                If occupied.Contains(ft.Item3) Then
+                    removeloc.Add(ft.Item3)
                 Else
-                    locations.Add(tpl, New List(Of Cart)({crt}))
+                    occupied.Add(ft.Item3)
+                End If
+            Next
+
+            'all fields where from-to is to-from
+            Dim removeid As New HashSet(Of Integer)
+            For Each ft In from_to
+                If occupied.Contains(ft.Item2) Then
+                    For Each ft1 In from_to
+                        If ft.Item2.Equals(ft1.Item3) And ft.Item3.Equals(ft1.Item2) Then
+                            removeid.Add(ft.Item1)
+                            removeid.Add(ft1.Item1)
+                        End If
+                    Next
+                End If
+            Next
+
+
+            Dim newcarts As New List(Of Cart)
+            For Each crt In carts
+                If removeid.Contains(crt.id) Or removeloc.Contains(New Tuple(Of Integer, Integer)(crt.x, crt.y)) Then
+                    If Not crashed Then
+                        crashCart = crt
+                        crashed = True
+                    End If
+                Else
                     newcarts.Add(crt)
                 End If
             Next
-            Dim removeId As New HashSet(Of Integer)
-            For Each tpl1 In remove
-                Dim l1 = locations(tpl1)
-                For Each crt2 In l1
-                    removeId.Add(crt2.id)
-                Next
-                locations.Remove(tpl1)
-                If prevlocations.ContainsKey(tpl1) Then removeId.Add(prevlocations(tpl1)(0).id)
-            Next
-            prevlocations = locations
-            carts = New List(Of Cart)()
-            For Each crt2 In newcarts
-                If Not removeId.Contains(crt2.id) Then carts.Add(crt2)
-            Next
+
+            carts = newcarts
             seconds += 1
         End While
         If Not crashCart Is Nothing Then
             Console.WriteLine("cart crashed after " & seconds & " seconds at location " & crashCart.x & "," & crashCart.y)
         End If
-
+        If carts.Count > 0 Then
+            Console.WriteLine("cart survived after " & seconds & " seconds at location " & carts(0).x & "," & carts(0).y)
+        End If
     End Sub
 
     Class Cart
