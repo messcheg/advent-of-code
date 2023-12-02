9bcf29c73e51259af4a2f8b15a62c1a6bf4c15da
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-02 07:53:20 +0100

    Prepare 2023 Day03

---
+++ AdventOfCode2023/Day03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using AocHelper;
+
+Run(@"..\..\..\example.txt", true);
+Run(@"E:\develop\advent-of-code-input\2023\day03.txt", false);
+
+void Run(string inputfile, bool isTest)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    long supposedanswer1 = 8;
+    long supposedanswer2 = 2286;
+
+    var S = File.ReadAllLines(inputfile).ToList();
+    long answer1 = 0;
+    long answer2 = 0;
+
+
+    Aoc.w(1, answer1, supposedanswer1, isTest);
+    Aoc.w(2, answer2, supposedanswer2, isTest);
+}
