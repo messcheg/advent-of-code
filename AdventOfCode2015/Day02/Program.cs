394b7c38eb1076ee726aaafe7ed40ce164ef8f1d
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-15 08:57:23 +0100

    Added 2015 Day02 Part 1 (In Rockstar)

--- AdventOfCode2015/Day02/Program.cs
+++ AdventOfCode2015/Day02/Program.cs
@@ -10,33 +10,44 @@ public class Program
 
 
     /* Let's rock with Santa!!!
+         The crowd says Ho ho ho
+        Shout the crowd
+    
+    */
 
-      Midnight takes your heart and your soul
-      While your heart is as high as your soul
-      Put your heart without your soul into your heart
+    /*
+        Rudolf wants love and attention
+        The crowd says x
+        Cut attention in the boat with the crowd
+        Let the wish be roll the boat
+        Cast the wish
+        Let hope be roll the boat
+        Cast hope 
+        Let faith be roll the boat
+        Cast faith
+        Let there be the wish of hope
+        Let christmas be the wish of faith
+        Put hope of faith into the book
+        Let our mind be there with the book, Christmas
+        Let your mind be there
+        If your mind is stronger than Christmas
+        Let your mind be Christmas
 
-      Give back your heart
+        If your mind is greater than the book
+        let your mind be the book
+
+        Let love be with our mind with your mind, our mind
+        Give it back
 
 
-      Desire is a lovestruck ladykiller
-      My world is nothing 
-      Fire is ice
-      Hate is water
-      Until my world is Desire,
-      Build my world up
-      If Midnight taking my world, Fire is nothing and Midnight taking my world, Hate is nothing
-      Shout "FizzBuzz!"
-      Take it to the top
+        Rock the rhythm with "2x3x4", "1x1x10"
+        The message is nothing
+        while the rhythm ain't silent
+        Roll the rhythm into the mood
+        Put Rudolf taking the message, the mood into the message
 
-      If Midnight taking my world, Fire is nothing
-      Shout "Fizz!"
-      Take it to the top
+        Whisper the message
 
-      If Midnight taking my world, Hate is nothing
-      Say "Buzz!"
-      Take it to the top
-
-      Whisper my world
     */
 
     public static void Run()
