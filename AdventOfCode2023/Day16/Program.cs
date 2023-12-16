54dcecfa9b1f738f2f6ef483d26d6f3a11adf672
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-16 08:06:46 +0100

    Changed 2023 Day16 but the V2 is slower than my first code

--- AdventOfCode2023/Day16/Program.cs
+++ AdventOfCode2023/Day16/Program.cs
@@ -34,28 +34,50 @@ void Run(string inputfile, bool isTest, long supposedanswer1, long supposedanswe
 
     var currentbeams = new Queue<(int tox, int toy, int fromd)>();
     currentbeams.Enqueue((0, 0, 0));
+    var map = new Dictionary<(char c, int d), int>();
+    map[('.', 0)] = 0;
+    map[('.', 1)] = 1;
+    map[('.', 2)] = 2;
+    map[('.', 3)] = 3;
+    map[('/', 0)] = 3;
+    map[('/', 1)] = 2;
+    map[('/', 2)] = 1;
+    map[('/', 3)] = 0;
+    map[('\\', 0)] = 1;
+    map[('\\', 1)] = 0;
+    map[('\\', 2)] = 3;
+    map[('\\', 3)] = 2;
+    map[('-', 0)] = 0;
+    map[('-', 1)] = 5;
+    map[('-', 2)] = 2;
+    map[('-', 3)] = 5;
+    map[('|', 0)] = 6;
+    map[('|', 1)] = 1;
+    map[('|', 2)] = 6;
+    map[('|', 3)] = 3;
 
-    answer1 = GetEnergy(S, currentbeams);
+
+    answer1 = GetEnergyV2(S, currentbeams, map);
 
     for (int i = 0; i < S.Count; i++)
     {
         currentbeams.Enqueue((0, i, 0));
-        var a1 = GetEnergy(S, currentbeams);
+        var a1 = GetEnergyV2(S, currentbeams, map);
         if (a1 > answer2) answer2 = a1;
 
         currentbeams.Enqueue((S.Count-1, i, 2));
-        a1 = GetEnergy(S, currentbeams);
+        a1 = GetEnergyV2(S, currentbeams, map);
         if (a1 > answer2) answer2 = a1;
     }
 
     for (int i = 0; i < S[0].Length; i++)
     {
         currentbeams.Enqueue((i, 0, 1));
-        var a1 = GetEnergy(S, currentbeams);
+        var a1 = GetEnergyV2(S, currentbeams, map);
         if (a1 > answer2) answer2 = a1;
 
         currentbeams.Enqueue((i, S[0].Length - 1, 3));
-        a1 = GetEnergy(S, currentbeams);
+        a1 = GetEnergyV2(S, currentbeams, map);
         if (a1 > answer2) answer2 = a1;
     }
 
@@ -119,4 +141,38 @@ static long GetEnergy(List<string> S, Queue<(int tox, int toy, int fromd)> curre
     }
 
     return visited.Select(v => (v.x, v.y)).ToHashSet().Count;
+}
+static long GetEnergyV2(List<string> S, Queue<(int tox, int toy, int fromd)> currentbeams, Dictionary<(char c, int d), int> map)
+{
+    var visited = new HashSet<(int x, int y, int fromdirection)>();
+    var move = new (int dx, int dy)[4] { (1, 0), (0, 1), (-1, 0), (0, -1) };
+
+    while (currentbeams.Count > 0)
+    {
+        var beam = currentbeams.Dequeue();
+        if (!visited.Contains(beam))
+        {
+            visited.Add(beam);
+            var newd = map[(S[beam.toy][beam.tox], beam.fromd)];
+
+            if (newd < 5) Addwork(currentbeams, move, beam, newd, S[0].Length, S.Count);
+            else
+            {
+                var nd1 = newd == 5 ? 0 : 1;
+                var nd2 = newd == 5 ? 2 : 3;
+                Addwork(currentbeams, move, beam, nd1, S[0].Length, S.Count);
+                Addwork(currentbeams, move, beam, nd2, S[0].Length, S.Count);
+            }
+        }
+    }
+
+    return visited.Select(v => (v.x, v.y)).ToHashSet().Count;
+
+    static void Addwork(Queue<(int tox, int toy, int fromd)> currentbeams, (int dx, int dy)[] move, (int tox, int toy, int fromd) beam, int newd, int maxx, int maxy)
+    {
+        int newx = beam.tox + move[newd].dx;
+        int newy = beam.toy + move[newd].dy;
+        if (0 <= newx && newx < maxx && newy >= 0 && newy < maxy)
+            currentbeams.Enqueue((newx, newy, newd));
+    }
 }
