122baa0ff8f2956fa91346c3df854e058bc0fd2d
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-17 23:58:50 +0100

    Prepared 2023 Day18

---
+++ AdventOfCode2023/Day18/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Globalization;
+using System.Net.Security;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
+using System.Security;
+using System.Security.Cryptography;
+using AocHelper;
+
+Run(@"..\..\..\example.txt", true, 0000, 0000);
+//Run(@"..\..\..\example1.txt", true, 0000, 0000);
+//Run(@"..\..\..\example2.txt", true, 22, 4);
+Run(@"E:\develop\advent-of-code-input\2023\day18.txt", false, 0, 0);
+
+void Run(string inputfile, bool isTest, long supposedanswer1, long supposedanswer2)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+
+    //var S = File.ReadAllText(inputfile).Split("\r\n\r\n").Select(s => s.Split("\r\n").ToList()).ToList();
+    //var S = File.ReadAllLines(inputfile).Select(a => a.Select(b => b - '0').ToList()).ToList();
+    var S = File.ReadAllLines(inputfile).ToList();
+ 
+    long answer1 = 0;
+    long answer2 = 0;
+
+
+
+    stopwatch.Stop();
+    if (supposedanswer1 > -1) Aoc.w(1, answer1, supposedanswer1, isTest);
+    if (supposedanswer2 > -1) Aoc.w(2, answer2, supposedanswer2, isTest);
+    Console.WriteLine("Time in miliseconds: " + stopwatch.ElapsedMilliseconds.ToString());
+}
