6bb504451a335557937910f2dc76c1bb2d41c985
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-18 22:32:05 +0100

    Added 2023 Day18 Part II

--- AdventOfCode2023/Day18/Program.cs
+++ AdventOfCode2023/Day18/Program.cs
@@ -18,7 +18,7 @@ using System.Security.Cryptography;
 using AocHelper;
 
 Run(@"..\..\..\example.txt", true, 62, 0000);
-Run(@"..\..\..\example1.txt", true, 61, 0000);
+//Run(@"..\..\..\example1.txt", true, 61, 0000);
 //Run(@"..\..\..\example2.txt", true, 22, 4);
 Run(@"E:\develop\advent-of-code-input\2023\day18.txt", true, 0, 0);
 
@@ -33,231 +33,173 @@ void Run(string inputfile, bool isTest, long supposedanswer1, long supposedanswe
     long answer1 = 0;
     long answer2 = 0;
 
-    var field = new Dictionary<(int x, int y), List<char>>();
-    var secondfield = new List<(long x, long y)>();
-    int x = 0;
-    int y = 0;
-    long x2 = 0; long y2 = 0;
-    int dir = 3;
-    foreach (var s in S)
-    {
-        var s1 = s.Split(" ").ToArray();
-        var d = s1[0];
-        var d2 = s1[2][6]-'0';
-        var cnt2 = Convert.ToInt64(s1[2].Substring(1, 5), 16);
-        var cnt = int.Parse(s1[1]);
-        void SetField(int x, int y, char value)
-        {
-            List<char> l;
-            if (field == null) return;
-            if (!field.TryGetValue((x,y),out l))
-            {
-                l = new List<char>();
-                field[(x, y)] = l ;
-            }
-            else
-            {
-                Console.WriteLine("overlap at: " + x.ToString() + ", " + y.ToString());
-            }
-            l.Add(value);
-        }
-        
+    var directions = "RDLU";
+    bool part1 = true;
 
-        switch (d)
-        {
-            case "U":
-                {
-                    var y1 = y - cnt;
-                    if (dir == 0) SetField(x,y,'J');
-                    else if (dir == 2) SetField(x,y, 'L');
-                    for (int i = y - 1; i > y1; i--) SetField(x, i, '|');
-                    y = y1;
-                    dir = 3;
-                    break;
-                }
-            case "D":
-                {
-                    var y1 = y + cnt;
-                    if (dir == 0) SetField(x,y, ')');
-                    else if (dir == 2) SetField(x,y, 'F');
-                    for (int i = y + 1; i < y1; i++) SetField(x,i, '|');
-                    y = y1;
-                    dir = 1;
-                    break;
-                }
-            case "L":
-                {
-                    var x1 = x - cnt;
-                    if (dir == 1) SetField(x,y, 'J');
-                    else if (dir == 3) SetField(x,y, ')');
-                    for (int i = x - 1; i > x1; i--) SetField(i, y, '-');
-                    x = x1;
-                    dir = 2;
-                    break;
-                }
-            case "R":
-                {
-                    var x1 = x + cnt;
-                    if (dir == 1) SetField(x,y, 'L');
-                    else if (dir == 3) SetField(x,y, 'F');
-                    for (int i = x + 1; i < x1; i++) SetField(i, y, '-');
-                    x = x1;
-                    dir = 0;
-                    break;
-                }
-        }
-        switch (d2)
-        {
-            case 0: x2 += cnt2; break;
-            case 1: y2 += cnt2; break;
-            case 2: x2 -= cnt2; break;
-            case 3: y2 -= cnt2; break;
-        }
-        secondfield.Add((x2, y2));
-    }
-    int maxy = field.Keys.Select(k => k.y).Max();
-    int maxx = field.Keys.Select(k => k.x).Max();
-    int miny = field.Keys.Select(k => k.y).Min();
-    int minx = field.Keys.Select(k => k.x).Min();
-
-    bool inside = false;
-    char enter = ' ';
-    for (int j=miny;j<=maxy;j++)
+    for (int parts = 0; parts < 2; parts++)
     {
+        var field = new List<(long x, long y, char type)>();
+        long x = 0;
+        long y = 0;
+        int dir = 3;
+        foreach (var s in S)
+        {
+            var s1 = s.Split(" ").ToArray();
+            var d = part1 ? s1[0][0] : directions[s1[2][7] - '0'];
+            var cnt = part1 ? long.Parse(s1[1]) : Convert.ToInt64(s1[2].Substring(2, 5), 16);
+
+            switch (d)
+            {
+                case 'U':
+                    {
+                        var y1 = y - cnt;
+                        if (dir == 0) field.Add((x, y, 'J'));
+                        else if (dir == 2) field.Add((x, y, 'L'));
+                        y = y1;
+                        dir = 3;
+                        break;
+                    }
+                case 'D':
+                    {
+                        var y1 = y + cnt;
+                        if (dir == 0) field.Add((x, y, ')'));
+                        else if (dir == 2) field.Add((x, y, 'F'));
+                        y = y1;
+                        dir = 1;
+                        break;
+                    }
+                case 'L':
+                    {
+                        var x1 = x - cnt;
+                        if (dir == 1) field.Add((x, y, 'J'));
+                        else if (dir == 3) field.Add((x, y, ')'));
+                        x = x1;
+                        dir = 2;
+                        break;
+                    }
+                case 'R':
+                    {
+                        var x1 = x + cnt;
+                        if (dir == 1) field.Add((x, y, 'L'));
+                        else if (dir == 3) field.Add((x, y, 'F'));
+                        x = x1;
+                        dir = 0;
+                        break;
+                    }
+            }
+        }
+
+        var ordererd = field.OrderBy(a => a.y).ThenBy(a => a.x).ToList();
+
+        bool inside = false;
+        char enter = ' ';
         inside = false;
-        for (int i =minx;i<=maxx;i++)
+        var curline = new List<long>();
+        long prevY = 0;
+        long answer = 0;
+        int cornerNr = 0;
+
+        while (cornerNr < ordererd.Count)
         {
-            if (field.ContainsKey((i, j)))
+            var corner = ordererd[cornerNr];
+            if (corner.y != prevY)
             {
-                var c = field[(i, j)];
-                answer1++;
-                switch (c[0])
+                var ysize = corner.y - prevY;
+                inside = false;
+                long prevX = 0;
+                foreach (var Xcorner in curline)
                 {
-                    case '|': inside = !inside;  break;
-                    case 'L': enter = 'L'; break;
-                    case 'F': enter = 'F'; break;
-                    case 'J':
-                        if (enter == 'F') inside = !inside;
-                        break;
-                    case ')':
-                        if (enter == 'L') inside = !inside;
-                        break;
+                    if (inside)
+                    {
+                        answer += (ysize - 1) * (Xcorner + 1 - prevX);
+                    }
+                    inside = !inside;
+                    prevX = Xcorner;
                 }
-               if (isTest) Console.Write(c[0]);
             }
-            else
+            prevY = corner.y;
+
+            var newline = new List<long>();
+            int xCnt = 0;
+            inside = false;
+            long prevX1 = 0;
+            while (cornerNr < ordererd.Count && ordererd[cornerNr].y == prevY && xCnt < curline.Count)
             {
-                if (inside) 
-                { 
-                    answer1++;
-                    if (isTest) Console.Write('#');
+                corner = ordererd[cornerNr];
+
+                if (corner.x <= curline[xCnt])
+                {
+                    HandleCorner(ref inside, ref enter, ref answer, cornerNr, corner, newline, ref prevX1);
+
+                    if (curline[xCnt] == corner.x) xCnt++;
+                    cornerNr++;
                 }
                 else
                 {
-                    if (isTest) Console.Write('.');
+                    HandleLine(ref inside, curline, ref answer, newline, xCnt, ref prevX1);
+                    xCnt++;
                 }
             }
-        }
-        if (isTest) Console.WriteLine();
-    }
-
-    var ordererd = secondfield.OrderBy(a => a.y).ThenBy(a=>a.x).ToList();
-    int edgeNr = 0;
-    var curline = new List<long>();
-    var lastline = curline;
-    long prevY = 0;
-    while (edgeNr < ordererd.Count)
-    {
-        var curEdge = ordererd[edgeNr];
-        var ysize = curEdge.y - prevY;
-        inside = false;
-        long prevX = 0;
-        foreach (var Xedge in curline)
-        {
-            if(inside) 
+            while (cornerNr < ordererd.Count && ordererd[cornerNr].y == prevY)
             {
-                answer2 += (ysize - 1) * (Xedge + 1 - prevX);
+                corner = ordererd[cornerNr];
+                HandleCorner(ref inside, ref enter, ref answer, cornerNr, corner, newline, ref prevX1);
+                cornerNr++;
             }
-            inside = !inside;
-            prevX = Xedge;
-        }
-        prevY = curEdge.y;
-        int xCnt = 0;
-
-        var newline = new List<long>();
-        inside = false;
-        bool curinside = false;
-        prevX = 0;
-        while (edgeNr < ordererd.Count && ordererd[edgeNr].y == prevY && xCnt < curline.Count) 
-        {
-            if (ordererd[edgeNr].x < curline[xCnt])
+            while (xCnt < curline.Count)
             {
-                if (!inside && !curinside)
-                {
-                    curinside = true;
-                    prevX = ordererd[edgeNr].x;
-                    newline.Add(ordererd[edgeNr].x);
-                }
-                else if (!inside && curinside)
-                {
-                    curinside = false;
-                    answer2 += ordererd[edgeNr].x + 1 - prevX;
-                    prevX = ordererd[edgeNr].x +1;
-                    newline.Add(ordererd[edgeNr].x);
-                }
-                else if (inside && curinside)
-                {
-                    prevX = ordererd[edgeNr].x;
-                    newline.Add(ordererd[edgeNr].x);
-                }
-                else if (inside && !curinside)
-                {
-
-                }
-                edgeNr++;
-            }
-            else if (ordererd[edgeNr].x > curline[xCnt])
-            {
+                HandleLine(ref inside, curline, ref answer, newline, xCnt, ref prevX1);
                 xCnt++;
             }
-            else
-            {
-            
-                edgeNr++;
-                xCnt++;
-            }
-        }
-        while (edgeNr < ordererd.Count)
-        {
-            newline.Add(ordererd[edgeNr].x);
-            edgeNr++;
-        }
-        while (xCnt < curline.Count)
-        {
-            edgeNr++;
-            xCnt++;
-        }
 
-        curline = newline;
+            curline = newline;
+        }
+        if (part1) answer1 = answer; else answer2 = answer;
+        part1 = !part1;
     }
-    if (lastline.Count > 0)
-    {
-        inside = false;
-        long prevX = 0;
-        foreach (var Xedge in curline)
-        {
-            if (inside)
-            {
-                answer2 += (Xedge + 1 - prevX);
-            }
-            inside = !inside;
-            prevX = Xedge;
-        }
-
-    }
-
     stopwatch.Stop();
     if (supposedanswer1 > -1) Aoc.w(1, answer1, supposedanswer1, isTest);
     if (supposedanswer2 > -1) Aoc.w(2, answer2, supposedanswer2, isTest);
     Console.WriteLine("Time in miliseconds: " + stopwatch.ElapsedMilliseconds.ToString());
+
+    static void HandleLine(ref bool inside, List<long> curline, ref long answer, List<long> newline, int xCnt, ref long prevX1)
+    {
+        var curX = curline[xCnt];
+        if (inside)
+        {
+            answer += curX - prevX1 + 1;
+            prevX1 = curX + 1;
+            inside = false;
+        }
+        else
+        {
+            prevX1 = curX;
+            inside = true;
+        }
+        newline.Add(curX);
+    }
+}
+
+static void HandleCorner(ref bool inside, ref char enter, ref long answer, int cornerNr, (long x, long y, char type) corner, List<long> newline, ref long prevX1)
+{
+    if (corner.type == 'F' || corner.type == ')') newline.Add(corner.x);
+    if (corner.type == 'F' || corner.type == 'L')
+    {
+        if (inside) answer += corner.x - prevX1;
+        enter = corner.type;
+        prevX1 = corner.x;
+    }
+    else if (corner.type == ')' && enter == 'F' ||
+        corner.type == 'J' && enter == 'L')
+    {
+        answer += corner.x - prevX1 + 1;
+        prevX1 = corner.x + 1;
+    }
+    else if (corner.type == ')' && enter == 'L' ||
+        corner.type == 'J' && enter == 'F')
+    {
+        answer += corner.x - prevX1 + 1;
+        prevX1 = corner.x + 1;
+        inside = !inside;
+    }
 }
