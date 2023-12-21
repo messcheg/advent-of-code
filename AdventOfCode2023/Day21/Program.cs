667eaa10318b6e0e6be5021e02259e5dda3ac809
Gert-Jan Messchendorp <messcheg@gmail.com>
2023-12-22 00:59:08 +0100

    Started with 2023 Day21 part II

--- AdventOfCode2023/Day21/Program.cs
+++ AdventOfCode2023/Day21/Program.cs
@@ -44,53 +44,97 @@ void Run(string inputfile, bool isTest, long supposedanswer1, long supposedanswe
         for (int j = 0; j < Y; j++)
             if (S[j][i] == 'S') (sx, sy) = (i, j);
     stepsset.Add((sx, sy));
-    
-    for (int i = 0; i< 100; i++)
+
+    for (int i = 0; i < 65; i++)
     {
         if (i == targetdist) answer1 = stepsset.Count;
         var newsteps = new HashSet<(long x, long y)>();
         foreach (var (x, y) in stepsset)
         {
-                var ix = (int)(x % X + X) % X;
-                var iy = (int)(y % Y + Y) % Y;
+            var ix = (int)(x % X + X) % X;
+            var iy = (int)(y % Y + Y) % Y;
 
-                var up = (iy + Y + Y - 1) % Y;
-                var down = (iy + Y + 1) % Y;
-                var left = (ix + X + X - 1) % X;
-                var right = (ix + X + 1) % X;
+            var up = (iy + Y + Y - 1) % Y;
+            var down = (iy + Y + 1) % Y;
+            var left = (ix + X + X - 1) % X;
+            var right = (ix + X + 1) % X;
 
-                if (S[iy][right] != '#') newsteps.Add((x + 1, y));
-                if (S[down][ix] != '#') newsteps.Add((x, y + 1));
-                if (S[iy][left] != '#') newsteps.Add((x - 1, y));
-                if (S[up][ix] != '#') newsteps.Add((x, y - 1));
+            if (S[iy][right] != '#') newsteps.Add((x + 1, y));
+            if (S[down][ix] != '#') newsteps.Add((x, y + 1));
+            if (S[iy][left] != '#') newsteps.Add((x - 1, y));
+            if (S[up][ix] != '#') newsteps.Add((x, y - 1));
         }
 
-        Console.SetCursorPosition(0, 0);
-        Console.WriteLine("Step: " + i.ToString());
-        for (int j = -30; j < 30; j++)
+        if (isTest && i < 35)
         {
-            for (int k = -50; k < 50; k++)
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Step: " + i.ToString());
+            for (int j = -30; j < 30; j++)
             {
-                var ix = (int)(k % X + X) % X;
-                var iy = (int)(j % Y + Y) % Y;
-
-                if (newsteps.Contains((k, j)))
+                for (int k = -50; k < 50; k++)
                 {
-                    var clr = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("O");
-                    Console.ForegroundColor=clr;
+                    var ix = (int)(k % X + X) % X;
+                    var iy = (int)(j % Y + Y) % Y;
+
+                    if (newsteps.Contains((k, j)))
+                    {
+                        var clr = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("O");
+                        Console.ForegroundColor = clr;
+                    }
+                    else Console.Write(S[iy][ix]);
+
                 }
-                else Console.Write(S[iy][ix]);
-
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
-
         stepsset = newsteps;
     }
 
-    // Try to find shortes path
+    // Try to flod unil a certain level
+    var visited = new HashSet<(long x, long y)>();
+    bool ready = false;
+    var work = new HashSet<(long x, long y)>();
+    work.Add((sx, sy));
+    long steps = 0;
+    long steps1 = 0;
+    long cnt1 = 0;
+    long cnt2 = 0;
+    long steps2 = 0;
+    while (steps2 == 0)
+    {
+        steps++;
+        var newwork = new HashSet<(long x, long y)>();
+        foreach (var pos in work)
+        {
+            visited.Add(pos);
+            var (x, y) = pos;
+
+            var ix = (int)(x % X + X) % X;
+            var iy = (int)(y % Y + Y) % Y;
+
+            var up = (iy + Y + Y - 1) % Y;
+            var down = (iy + Y + 1) % Y;
+            var left = (ix + X + X - 1) % X;
+            var right = (ix + X + 1) % X;
+
+            if (S[iy][right] != '#' && !visited.Contains((x + 1, y))) newwork.Add((x + 1, y));
+            if (S[down][ix] != '#' && !visited.Contains((x, y + 1))) newwork.Add((x, y + 1));
+            if (S[iy][left] != '#' && !visited.Contains((x - 1, y))) newwork.Add((x - 1, y));
+            if (S[up][ix] != '#' && !visited.Contains((x, y - 1))) newwork.Add((x, y - 1));
+
+            if (steps1 == 0 && x == 5 * X - 1) steps1 = steps;
+            if (x == -4 * X) steps2 = steps;
+        }
+        if (steps1 == steps) cnt1 = visited.Count(a=> (a.x + a.y) % 2 == 0);
+        if (steps2 == steps) cnt2 = visited.Count(a => (a.x + a.y) % 2 == 0);
+        work = newwork; 
+    }
+
+    var rest  = 26501365 % steps2;
+    var div = 26501365 / steps2;
+
 
 
     // 26501365
