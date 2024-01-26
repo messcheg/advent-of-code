48f6903a3839e8ee69f48e9d1e25a255f2770369
Gert-Jan Messchendorp <gertjanmesschendorp@nl.ccv.eu>
2024-01-27 00:57:42 +0100

    Added 2015 Day4 and Day5 Couldn't do day4 in ROckstaar, MD5 calculation became to complex

---
+++ AdventOfCode2015/Day04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+ static string CalculateMD5Hash(string key, long number)
+{
+    var input = key + number.ToString();
+    using (MD5 md5 = MD5.Create())
+    {
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            sb.Append(hashBytes[i].ToString("x2"));
+        }
+        return sb.ToString();
+    }
+}
+
+//Console.WriteLine(CalculateMD5Hash("abcdef",609043));
+
+var key = "yzbqklnj";
+long i = 0;
+while (!CalculateMD5Hash(key, i).StartsWith("00000")) i++;
+Console.WriteLine("Answer1: " + i.ToString());
+
+i = 0;
+while (!CalculateMD5Hash(key, i).StartsWith("000000"))
+{
+   // Console.WriteLine(CalculateMD5Hash(key, i));
+    i++;
+}
+Console.WriteLine("Answer2: " + i.ToString());
