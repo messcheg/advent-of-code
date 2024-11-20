9e275ee24dbd273753ddaf4bc8166d2671fdfddb
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-11-20 22:30:49 +0100

    Prepare 2024

---
+++ AdventOfCode2024/AocHelper/Aoc.cs
@@ -0,0 +1,25 @@
+﻿namespace AocHelper
+{
+    public static class Aoc
+    {
+        public static void w<T>(int number, T val, T supposedval, bool isTest)
+        {
+            string? v = (val == null) ? "(null)" : val.ToString();
+            string? sv = (supposedval == null) ? "(null)" : supposedval.ToString();
+
+            var previouscolour = Console.ForegroundColor;
+            Console.Write("Answer Part " + number + ": ");
+            Console.ForegroundColor = (v == sv || !isTest) ? ConsoleColor.Green : ConsoleColor.Red;
+            Console.Write(v);
+            Console.ForegroundColor = previouscolour;
+            if (isTest)
+            {
+                Console.Write(" ... supposed (example) answer: ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(sv);
+                Console.ForegroundColor = previouscolour;
+            }
+            Console.WriteLine();
+        }
+    }
+}
