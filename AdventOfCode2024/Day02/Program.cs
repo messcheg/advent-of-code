1029ea9e77ee273feb04fc1435905ac2dcc2b53f
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-02 07:17:10 +0100

    Added 2024 day 2

--- AdventOfCode2024/Day02/Program.cs
+++ AdventOfCode2024/Day02/Program.cs
@@ -8,17 +8,80 @@ Run(@"..\..\..\example1.txt", true);
 void Run(string inputfile, bool isTest)
 {
     Stopwatch stopwatch = Stopwatch.StartNew();
-    long supposedanswer1 = 0;
-    long supposedanswer2 = 0;
+    long supposedanswer1 = 2;
+    long supposedanswer2 = 4;
 
-    var S = File.ReadAllLines(inputfile).ToList();
+    var S = File.ReadAllLines(inputfile).Select(a => a.Split(' ').Select(b => long.Parse(b)).ToList()).ToList();
     long answer1 = 0;
     long answer2 = 0;
     foreach (var s in S)
     {
+        (bool, int) test(List<long> s)
+        {
+            bool up = s[0] <= s[1];
+            for (int i = 1; i < s.Count; i++)
+            {
+                long dif = 0;
+                if (up)
+                {
+                    dif = s[i] - s[i - 1];
+                }
+                else
+                {
+                    dif = s[i - 1] - s[i];
+                }
+                if (dif < 1 || dif > 3)
+                {
+                    return (false, i);
+                }
+            }
+            return (true, 0);
+        }
+
+        bool safe; int location = 0;
+        (safe, location) = test(s);
+        if (safe)
+        {
+            answer1++;
+            answer2++;
+        }
+        else
+        {
+            var s1 = s.ToList();
+            int l1;
+            s1.RemoveAt(location - 1);
+            (safe, l1) = test(s1);
+            if (safe)
+            {
+                answer2++;
+            }
+            else
+            {
+                s1 = s.ToList();
+                s1.RemoveAt(location);
+                (safe, l1) = test(s1);
+                if (safe)
+                {
+                    answer2++;
+                }
+                else
+                {
+                    if (location > 1)
+                    {
+                        s1 = s.ToList();
+                        s1.RemoveAt(location - 2);
+                        (safe, l1) = test(s1);
+                    }
+                    if (safe)
+                    {
+                        answer2++;
+
+                    }
+                }
+            }
+        }
 
     }
-
 
     Aoc.w(1, answer1, supposedanswer1, isTest);
     Aoc.w(2, answer2, supposedanswer2, isTest);
