d07e23b18d06381044c8e360636f8d26441e1817
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-03 06:56:05 +0100

    Where do I go wrong?

--- AdventOfCode2024/Day03/Program.cs
+++ AdventOfCode2024/Day03/Program.cs
@@ -8,19 +8,62 @@ Run(@"..\..\..\example1.txt", true);
 void Run(string inputfile, bool isTest)
 {
     Stopwatch stopwatch = Stopwatch.StartNew();
-    long supposedanswer1 = 0;
+    long supposedanswer1 = 161;
     long supposedanswer2 = 0;
 
-    var S = File.ReadAllLines(inputfile).ToList();
-    long answer1 = 0;
+    var S = File.ReadAllText(inputfile).Split("mul(").Select(a => a.Split(')').Select(b => b.Split(',').ToList()).ToList()).ToList();
+    ulong answer1 = 0;
     long answer2 = 0;
-    foreach (var s in S)
-    {
 
+    bool alldigits(string a)
+    {
+        foreach (var c in a)
+        {
+            if (c <= '0' || c >= '9') return false;
+        }
+        return true;
     }
 
+    /*
+        foreach (var s in S)
+        {
+            var pair = s[0];
+            if (pair.Count == 2)
+            {
+                if (alldigits(pair[0]) && alldigits(pair[1]) &&
+                    pair[0].Length <= 3 && pair[1].Length <= 3 &&
+                    pair[0].Length >= 1 && pair[1].Length >= 1) answer1 += ulong.Parse(pair[0]) * ulong.Parse(pair[1]);
+            }
+        }
+    */
 
-    Aoc.w(1, answer1, supposedanswer1, isTest);
+    var S1 = File.ReadAllText(inputfile);
+    int index = S1.IndexOf("mul(");
+    while (index > -1)
+    {
+        index += 4;
+        var index1 = S1.IndexOf(",", index);
+        if (index1 > -1)
+        {
+            var val1 = S1.Substring(index, index1 - index);
+            if (val1.Length > 0 && val1.Length <= 3 && alldigits(val1))
+            {
+                index = index1 + 1;
+                var index2 = S1.IndexOf(")", index);
+                if (index2 > -1)
+                {
+                    var val2 = S1.Substring(index, index2 - index);
+                    if (val2.Length > 0 && val2.Length <= 3 && alldigits(val2))
+                    {
+                        answer1 += ulong.Parse(val1) * ulong.Parse(val2);
+                    }
+                }
+            }
+        }
+        index = S1.IndexOf("mul(", index);
+    }
+
+    Aoc.w(1, answer1, (ulong)supposedanswer1, isTest);
     Aoc.w(2, answer2, supposedanswer2, isTest);
     Console.WriteLine("Duration: " + stopwatch.ElapsedMilliseconds.ToString() + " miliseconds.");
 }
