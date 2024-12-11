b2c9ca85f25b563e57cd66d204352858dc0b0f09
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-11 10:10:54 +0100

    Added 2025 Day11 part 2

--- AdventOfCode2024/Day11/Program.cs
+++ AdventOfCode2024/Day11/Program.cs
@@ -10,68 +10,45 @@ Run(@"..\..\..\input.txt", false, 75);
 void Run(string inputfile, bool isTest, int numberoftimes, long supposedanswer1 = 0)
 {
     Stopwatch stopwatch = Stopwatch.StartNew();
-    long supposedanswer2 = 0;
 
     var S = File.ReadAllText(inputfile).Split(' ').Select(a => long.Parse(a)).ToList();
     long answer1 = 0;
-    long answer2 = 0;
-    var prework = new long[numberoftimes];
 
-    var oldlist = new List<long>() { 0 };
-    for (int i = 0; i < numberoftimes; i++)
+    var cache = new Dictionary<(long number, int numberoftimes), long>();
+    long Getstones(long number, int times)
     {
-        var newlist = new List<long>();
-        for (int j = 0; j < oldlist.Count; j++)
-        {
-            var cur = oldlist[j];
-            if (cur == 0) { newlist.Add(1); continue; }
-            var scur = cur.ToString();
-            if (scur.Length % 2 == 0)
-            {
-                newlist.Add(long.Parse(scur.Substring(0, scur.Length / 2)));
-                newlist.Add(long.Parse(scur.Substring(scur.Length / 2)));
-                continue;
-            }
-            newlist.Add(cur * 2024);
-        }
-        oldlist = newlist;
-        prework[i] = oldlist.Count;
-    }
-
-
-
-
-    var work = new Stack<(long number, int numberoftimes)>();
-    for (int i = S.Count - 1; i >= 0; i--)
-    {
-        work.Push((S[i], numberoftimes));
-    }
-
-    while (work.Count > 0)
-    {
-        var w = work.Pop();
-        if (w.numberoftimes == 0) answer1++;
+        if (times == 0) return 1;
         else
         {
-            if (w.number == 0) answer1 += prework[w.numberoftimes - 1];
+            if (cache.TryGetValue((number, times), out long result)) return result;
+            if (number == 0)
+            {
+                result = Getstones(1, times - 1);
+            }
             else
             {
-                var scur = w.number.ToString();
+                var scur = number.ToString();
                 if (scur.Length % 2 == 0)
                 {
-                    work.Push((long.Parse(scur.Substring(scur.Length / 2)), w.numberoftimes - 1));
-                    work.Push((long.Parse(scur.Substring(0, scur.Length / 2)), w.numberoftimes - 1));
+                    result += Getstones(long.Parse(scur.Substring(0, scur.Length / 2)), times - 1);
+                    result += Getstones(long.Parse(scur.Substring(scur.Length / 2)), times - 1);
                 }
                 else
                 {
-                    work.Push((w.number * 2024, w.numberoftimes - 1));
+                    result = Getstones(number * 2024, times - 1);
                 }
             }
+            cache[(number, times)] = result;
+            return result;
         }
     }
 
+    foreach (var number in S)
+    {
+        answer1 += Getstones(number, numberoftimes);
+    }
+
     Aoc.w(1, answer1, supposedanswer1, isTest);
-    Aoc.w(2, answer2, supposedanswer2, isTest);
     Console.WriteLine("Duration: " + stopwatch.ElapsedMilliseconds.ToString() + " miliseconds.");
 }
 
