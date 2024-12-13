663f6cd1892e51a08e004c748f6b206dab87cbbd
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-13 07:07:28 +0100

    Added 2024 Day13

--- AdventOfCode2024/Day13/Program.cs
+++ AdventOfCode2024/Day13/Program.cs
@@ -8,17 +8,42 @@ Run(@"..\..\..\example1.txt", true);
 void Run(string inputfile, bool isTest)
 {
     Stopwatch stopwatch = Stopwatch.StartNew();
-    long supposedanswer1 = 0;
+    long supposedanswer1 = 480;
     long supposedanswer2 = 0;
 
     var S = File.ReadAllLines(inputfile).ToList();
     long answer1 = 0;
     long answer2 = 0;
-    foreach (var s in S)
+    int i = 0;
+    while (i < S.Count)
     {
+        var s = S[i].Split(", ").Select(a => long.Parse(a.Split("+").Last())).ToArray();
+        var x1 = s[0];
+        var y1 = s[1];
+        i++;
 
+        s = S[i].Split(", ").Select(a => long.Parse(a.Split("+").Last())).ToArray();
+        var x2 = s[0];
+        var y2 = s[1];
+        i++;
+
+        s = S[i].Split(", ").Select(a => long.Parse(a.Split("=").Last())).ToArray();
+        var x = s[0];
+        var y = s[1];
+        i++;
+        i++;
+
+        var B = (y * x1 - x * y1) / (y2 * x1 - x2 * y1);
+        var A = (x - B * x2) / x1;
+
+        if (A >= 0 && B >= 0 && B * x2 + A * x1 == x && B * y2 + A * y1 == y) answer1 += A * 3 + B;
+
+        x += 10000000000000;
+        y += 10000000000000;
+        B = (y * x1 - x * y1) / (y2 * x1 - x2 * y1);
+        A = (x - B * x2) / x1;
+        if (A >= 0 && B >= 0 && B * x2 + A * x1 == x && B * y2 + A * y1 == y) answer2 += A * 3 + B;
     }
-
 
     Aoc.w(1, answer1, supposedanswer1, isTest);
     Aoc.w(2, answer2, supposedanswer2, isTest);
