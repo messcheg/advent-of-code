19a244121f2020a7cf91c9c1481b30e4e011ee48
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-18 10:38:05 +0100

    2024 Day 18 faster version

--- AdventOfCode2024/Day18/Program.cs
+++ AdventOfCode2024/Day18/Program.cs
@@ -9,17 +9,21 @@ void Run(string inputfile, bool isTest, int size, int down, bool show)
 {
     Stopwatch stopwatch = Stopwatch.StartNew();
     long supposedanswer1 = 22;
-    long supposedanswer2 = 0;
 
     var coordinates = File.ReadAllLines(inputfile).Select(s => s.Split(',').Select(int.Parse).ToArray()).Select(a => (x: a[0], y: a[1])).ToArray();
     long answer1 = 0;
     (int x, int y) answer2 = (0, 0);
-    var corrupt = coordinates[..down].ToHashSet();
+    var useless = new bool[size, size];
+    for (int i = 0; i < down; i++)
+    {
+        var c = coordinates[i];
+        useless[c.x, c.y] = true;
+    }
     for (int j = 0; j < size; j++)
     {
         for (int i = 0; i < size; i++)
         {
-            if (corrupt.Contains((i, j))) Console.Write('#');
+            if (useless[i, j]) Console.Write('#');
             else
                 Console.Write('.');
         }
@@ -28,43 +32,55 @@ void Run(string inputfile, bool isTest, int size, int down, bool show)
 
     for (int k = down - 1; k < coordinates.Length; k++)
     {
-        corrupt.Add(coordinates[k]);
+        var c = coordinates[k];
 
-        var work = new PriorityQueue<(int x, int y), int>();
-        work.Enqueue((0, 0), 0);
-        var visited = new HashSet<(int x, int y)>();
-        var state = new Dictionary<(int x, int y), int>();
-        state.Add((0, 0), 0);
-        long answer = 0;
-        while (work.Count > 0 && answer == 0)
+        if (!useless[c.x, c.y] || k < down)
         {
-            var cur = work.Dequeue();
-            visited.Add(cur);
-            if (cur == (size - 1, size - 1))
+            useless[c.x, c.y] = true;
+
+            var work = new PriorityQueue<(int x, int y), int>();
+            work.Enqueue((0, 0), 0);
+            var state = new (bool visited, bool seen, int costs)[size, size];
+            long answer = 0;
+            while (work.Count > 0)
             {
-                answer = state[cur];
-                break;
-            }
-            else
-            {
-                void addWOrk(int x, int y, int cost)
+                var cur = work.Dequeue();
+                state[cur.x, cur.y].visited = true;
+                if (cur == (size - 1, size - 1) && answer == 0)
                 {
-                    if (x < 0 || y < 0 || x >= size || y >= size) return;
-                    if (corrupt.Contains((x, y))) return;
-                    if (!state.ContainsKey((x, y)))
-                    {
-                        state[(x, y)] = cost;
-                        work.Enqueue((x, y), cost);
-                    }
+                    answer = state[cur.x, cur.y].costs;
                 }
-                addWOrk(cur.x - 1, cur.y, state[cur] + 1);
-                addWOrk(cur.x + 1, cur.y, state[cur] + 1);
-                addWOrk(cur.x, cur.y - 1, state[cur] + 1);
-                addWOrk(cur.x, cur.y + 1, state[cur] + 1);
+                else
+                {
+                    void addWOrk(int x, int y)
+                    {
+                        if (x < 0 || y < 0 || x >= size || y >= size) return;
+                        if (useless[x, y]) return;
+
+                        if (!state[x, y].seen)
+                        {
+                            state[x, y].seen = true;
+                            var cost = state[x, y].costs = state[cur.x, cur.y].costs + 1;
+                            work.Enqueue((x, y), cost);
+                        }
+                    }
+                    addWOrk(cur.x - 1, cur.y);
+                    addWOrk(cur.x + 1, cur.y);
+                    addWOrk(cur.x, cur.y - 1);
+                    addWOrk(cur.x, cur.y + 1);
+                }
             }
+            if (k == down - 1)
+            {
+                answer1 = answer;
+            }
+            else if (answer == 0) { answer2 = coordinates[k]; break; }
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    if (!state[i, j].seen) useless[i, j] = true;
+                }
         }
-        if (k == down) { answer1 = answer; }
-        else if (answer == 0) { answer2 = coordinates[k]; break; }
     }
 
     Aoc.w(1, answer1, supposedanswer1, isTest);
