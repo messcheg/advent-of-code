015406cf0bb8595099206a7aa4868a7a966e27f7
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2024-12-19 00:11:55 +0100

    2024 Day 18 finally in 200ms.

--- AdventOfCode2024/Day18/Program.cs
+++ AdventOfCode2024/Day18/Program.cs
@@ -13,73 +13,85 @@ void Run(string inputfile, bool isTest, int size, int down, bool show)
     var coordinates = File.ReadAllLines(inputfile).Select(s => s.Split(',').Select(int.Parse).ToArray()).Select(a => (x: a[0], y: a[1])).ToArray();
     long answer1 = 0;
     (int x, int y) answer2 = (0, 0);
-    var useless = new bool[size, size];
+    var state = new (int seen, bool useless, int costs)[size, size];
     for (int i = 0; i < down; i++)
     {
         var c = coordinates[i];
-        useless[c.x, c.y] = true;
+        state[c.x, c.y].useless = true;
     }
     for (int j = 0; j < size; j++)
     {
         for (int i = 0; i < size; i++)
         {
-            if (useless[i, j]) Console.Write('#');
+            if (state[i, j].useless) Console.Write('#');
             else
                 Console.Write('.');
         }
         Console.WriteLine();
     }
-
+    var end = (size - 1, size - 1);
     for (int k = down - 1; k < coordinates.Length; k++)
     {
         var c = coordinates[k];
 
-        if (!useless[c.x, c.y] || k < down)
+        if (!state[c.x, c.y].useless || k < down)
         {
-            useless[c.x, c.y] = true;
+            state[c.x, c.y].useless = true;
+            bool effective = c.x == 0 || c.y == 0 || c.x == size - 1 || c.y == size - 1 ||
+                 state[c.x - 1, c.y].useless ||
+                 state[c.x + 1, c.y].useless ||
+                 state[c.x - 1, c.y - 1].useless ||
+                 state[c.x + 1, c.y - 1].useless ||
+                 state[c.x - 1, c.y + 1].useless ||
+                 state[c.x + 1, c.y + 1].useless ||
+                 state[c.x, c.y - 1].useless ||
+                 state[c.x, c.y + 1].useless;
 
-            var work = new PriorityQueue<(int x, int y), int>();
-            work.Enqueue((0, 0), 0);
-            var state = new (bool visited, bool seen, int costs)[size, size];
-            long answer = 0;
-            while (work.Count > 0)
+            if (effective)
             {
-                var cur = work.Dequeue();
-                state[cur.x, cur.y].visited = true;
-                if (cur == (size - 1, size - 1) && answer == 0)
+                var work = new PriorityQueue<(int x, int y), int>();
+                work.Enqueue((0, 0), 0);
+                long answer = 0;
+                while (work.Count > 0)
                 {
-                    answer = state[cur.x, cur.y].costs;
-                }
-                else
-                {
-                    void addWOrk(int x, int y)
+                    var cur = work.Dequeue();
+                    if (cur == end && answer == 0)
                     {
-                        if (x < 0 || y < 0 || x >= size || y >= size) return;
-                        if (useless[x, y]) return;
-
-                        if (!state[x, y].seen)
-                        {
-                            state[x, y].seen = true;
-                            var cost = state[x, y].costs = state[cur.x, cur.y].costs + 1;
-                            work.Enqueue((x, y), cost);
-                        }
+                        answer = state[cur.x, cur.y].costs;
                     }
-                    addWOrk(cur.x - 1, cur.y);
-                    addWOrk(cur.x + 1, cur.y);
-                    addWOrk(cur.x, cur.y - 1);
-                    addWOrk(cur.x, cur.y + 1);
+                    else
+                    {
+                        int addWork(int x, int y)
+                        {
+                            if (x < 0 || y < 0 || x >= size || y >= size) return 1;
+                            var s = state[x, y];
+                            if (s.useless) return 1;
+                            if (s.seen == k) return 0;
+                            s.seen = k;
+                            s.costs = state[cur.x, cur.y].costs + 1;
+                            work.Enqueue((x, y), s.costs);
+                            state[x, y] = s;
+                            return 0;
+                        }
+                        int blocked = addWork(cur.x - 1, cur.y);
+                        blocked += addWork(cur.x + 1, cur.y);
+                        blocked += addWork(cur.x, cur.y - 1);
+                        blocked += addWork(cur.x, cur.y + 1);
+                        if (blocked == 3) state[cur.x, cur.y].useless = true;
+                    }
                 }
-            }
-            if (k == down - 1)
-            {
-                answer1 = answer;
-            }
-            else if (answer == 0) { answer2 = coordinates[k]; break; }
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
+                if (k == down - 1)
                 {
-                    if (!state[i, j].seen) useless[i, j] = true;
+                    answer1 = answer;
                 }
+                else if (answer == 0) { answer2 = coordinates[k]; break; }
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                    {
+                        var s = state[i, j];
+                        if (!s.useless && s.seen != k) state[i, j].useless = true;
+                    }
+            }
         }
     }
 
