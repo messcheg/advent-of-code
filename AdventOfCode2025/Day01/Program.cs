fa9a66510c2a260541e602c00e4c9cf8923202bb
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2025-11-27 23:55:40 +0100

    Prepare 2025

---
+++ AdventOfCode2025/Day01/Program.cs
@@ -0,0 +1,25 @@
+﻿using AocHelper;
+using System.Diagnostics;
+
+Run(@"..\..\..\example.txt", true);
+Run(@"..\..\..\input.txt", false);
+
+void Run(string inputfile, bool isTest)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    long supposedanswer1 = 0;
+    long supposedanswer2 = 0;
+
+    var S = File.ReadAllLines(inputfile).ToList();
+    var A = new List<long>();
+    var B = new List<long>();
+    long answer1 = 0;
+    long answer2 = 0;
+
+
+
+    Aoc.w(1, answer1, supposedanswer1, isTest);
+    Aoc.w(2, answer2, supposedanswer2, isTest);
+    Console.WriteLine("Duration: " + stopwatch.ElapsedMilliseconds.ToString() + " miliseconds.");
+}
+
