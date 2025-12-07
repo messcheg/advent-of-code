c951bd93b46195fe5eb4ee38e0b7c3b51c5aa78b
Gert-Jan Messchendorp <GertJanMesschendorp@nl.ccv.eu>
2025-12-07 07:05:02 +0100

    Added 2025 Day07

---
+++ AdventOfCode2025/Day07/Program.cs
@@ -0,0 +1,50 @@
+﻿using AocHelper;
+using System.Diagnostics;
+using System.Numerics;
+
+Run(@"..\..\..\example.txt", true);
+Run(@"..\..\..\input.txt", false);
+
+void Run(string inputfile, bool isTest)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    long supposedanswer1 = 21;
+    long supposedanswer2 = 40;
+
+    var S = File.ReadAllLines(inputfile).ToList();
+    long answer1 = 0;
+    long answer2 = 0;
+    var timelines = new long[S[0].Length];
+    for (int j = 0; j < S.Count; j++)
+    {
+        var newtimelines = new long[S[0].Length];
+        for (int i = 0; i < S[j].Length; i++)
+        {
+            switch (S[j][i])
+            {
+                case 'S':
+                    newtimelines[i] = 1;
+                    break;
+                case '^':
+                    var tl = timelines[i];
+                    if (tl > 0)
+                    {
+                        answer1++;
+                        newtimelines[i - 1] += tl;
+                        newtimelines[i + 1] += tl;
+                    }
+                    break;
+                default:
+                    newtimelines[i] += timelines[i];
+                    break;
+            }
+        }
+        timelines = newtimelines;
+    }
+    answer2 = timelines.Sum();
+
+    Aoc.w(1, answer1, supposedanswer1, isTest);
+    Aoc.w(2, answer2, supposedanswer2, isTest);
+    Console.WriteLine("Duration: " + stopwatch.ElapsedMilliseconds.ToString() + " miliseconds.");
+}
+
